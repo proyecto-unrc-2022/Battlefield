c26cd8adb332969b438116e90cbdc039cd5674bf
Franco Brusatti <fbrusatti@gmail.com>
2022-09-08 11:03:58 -0300

    Create basic blueprint

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+from auth import auth as auth_blueprint
+
+app.register_blueprint(auth_blueprint, url_prefix='/auth')
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
