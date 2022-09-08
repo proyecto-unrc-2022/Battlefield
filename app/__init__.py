5f977afaa4c1f78b871082499f6615a87ef01d95
Franco Brusatti <fbrusatti@gmail.com>
2022-09-08 11:53:19 -0300

    Connect with DB

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
-from ..config import config
+from config import config
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 def create_app(environment='development'):
     app = Flask(__name__)
@@ -7,8 +11,12 @@ def create_app(environment='development'):
     app.config.from_object(config[environment])
     config[environment].init_app(app)
 
-    from ..auth import auth as auth_blueprint
-    from ..api.v1.air_force import air_force as air_force_blueprint
+    db.init_app(app)
+
+    from auth import auth as auth_blueprint
+    from api.v1.air_force import air_force as air_force_blueprint
+
+    from app.models.user import User
 
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
     app.register_blueprint(air_force_blueprint, url_prefix='/api/v1/air_force')
