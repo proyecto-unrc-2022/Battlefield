56b10ec4ce529ffde40474f22f1898bb50fe1c24
AFungo <augustofungo@gmail.com>
2022-09-22 11:21:30 -0300

    I move the class air_force_game to a new model file ande delete from model planes

--- app/models/airforce/plane.py
+++ app/models/airforce/plane.py
@@ -3,13 +3,6 @@ from marshmallow_sqlalchemy.fields import Nested
 from sqlalchemy.orm import relationship
 
 from app import db
-from app.models.user import User
-
-
-class AirForceGame(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    player_a_id = db.Column(db.Integer, db.ForeignKey(User.id))
-    player_b_id = db.Column(db.Integer, db.ForeignKey(User.id))
 
 
 class Plane(db.Model):
