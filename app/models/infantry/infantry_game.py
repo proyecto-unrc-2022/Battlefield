c0517cff0bc10a9a54e8ddd04310c462c79d02e8
JoseCompag <joseic2000@gmail.com>
2022-09-27 19:13:43 -0300

    add models-infantry

--- app/models/infantry/infantry_game.py
+++ app/models/infantry/infantry_game.py
@@ -1,3 +1,4 @@
+#from turtle import back
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema, SQLAlchemySchema, auto_field
 from marshmallow_sqlalchemy.fields import Nested
 from sqlalchemy.orm import relationship #investigar
@@ -6,43 +7,71 @@ from sqlalchemy.orm import relationship #investigar
 from app import db
 from app.models.user import User
 
-class Game_Infantry(db.model):
-
-    __tablename__ = "game"
+class Game_Infantry(db.Model):
+    __tablename__ = "game_infantry"
 
     id = db.Column(db.Integer, primary_key=True)
-    id_player1 = db.Column(db.Integer, db.ForeignKey("users.id"))
-    id_player2 = db.Column(db.Integer, db.ForeignKey("users.id"))
+    id_user1 = db.Column(db.Integer, db.ForeignKey(User.id))
+    id_user2 = db.Column(db.Integer, db.ForeignKey(User.id))
+    
+    user_1 = relationship("User", foreign_keys=[id_user1])
+    user_2 = relationship("User", foreign_keys=[id_user2])
 
+    def __init__(self, id_user1=None, id_user2=None):
+        self.id_user1 = id_user1
+        self.id_user2 = id_user2    
 
+#Mayuscula
 class figure_infantry(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
-    id_user = db.Column(db.Integer, db.ForeignKey("User.id"), unique=True)
-    #id_proyectil = db.Column(db.Integer, unique=True) Sacar!
-    #id_game = db.Column(db.Integer, db.ForeignKey(""), unique=True)
-    #hp = db.Column(db.Integer, nullable=False)
-    #velocidad = db.Column(db.Integer, nullable=False)
-    #tamaño = db.Column(db.Integer, nullable=False)
-    #direccion = db.Column(db.String(2), nullable=False)
-    #pos_x = db.Column(db.Integer, nullable=False)
-    #pos_y = db.Column(db.Integer, nullable=False)
-    
+    id_user = db.Column(db.Integer, db.ForeignKey(User.id), unique=True)
+    #id_projectile = db.Column(db.Integer, unique=True) Sacar!
+    id_game = db.Column(db.Integer, db.ForeignKey(Game_Infantry.id), unique=True)
+    hp = db.Column(db.Integer, nullable=False)
+    velocidad = db.Column(db.Integer, nullable=False)
+    tamaño = db.Column(db.Integer, nullable=False)
+    direccion = db.Column(db.Integer, nullable=False)
+    pos_x = db.Column(db.Integer, nullable=False)
+    pos_y = db.Column(db.Integer, nullable=False)
 
-    #def __init__(self, id_user,id_game, hp, velocidad, tamaño, direccion, pos_x, pos_y):
+    game = relationship("Game_Infantry", foreign_keys=[id_game])
+
+    user = relationship("User", foreign_keys=[id_user])
+    
+    #user = relationship("User", backref=)
+
+    def __init__(self, id_user=None, id_game=None, hp=None, velocidad=None, tamaño=None, direccion=None, pos_x=None, pos_y=None):
+        self.id_user = id_user #game.user_1 preguntar como pasar  
+        self.id_game = id_game #game preguntar como pasar 
+        self.hp = hp
+        self.velocidad = velocidad
+        self.tamaño = tamaño
+        self.direccion = direccion
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+
 
 
 class Projectile(db.Model):
     
     id = db.Column(db.Integer, primary_key=True)
-    #id_game = db.Column(db.Integer, db.ForeignKey(""), unique=True)
-    #pos_x = db.Column(db.Integer, nullable=False)
-    #pos_y = db.Column(db.Integer, nullable=False)
-    #duracion = db.Column(db.Integer, nullable=False)
-    #daño = db.Column(db.Integer, nullable=False)
-    #direccion = db.Column(db.String(2), nullable=False)
+    id_game = db.Column(db.Integer, db.ForeignKey(Game_Infantry.id), unique=True)
+    pos_x = db.Column(db.Integer, nullable=False)
+    pos_y = db.Column(db.Integer, nullable=False)
+    velocidad = db.Column(db.Integer, nullable=False)
+    daño = db.Column(db.Integer, nullable=False)
+    direccion = db.Column(db.Integer, nullable=False)
 
-    #def __init__(self,)
+    game = relationship("Game_Infantry", foreign_keys=[id_game])
+
+    def __init__(self, id_game=None, pos_x=None, pos_y=None, velocidad=None, daño=None, direccion=None):
+        self.id_game = id_game
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.velocidad = velocidad
+        self.daño = daño
+        self.direccion = direccion
 
 
 
