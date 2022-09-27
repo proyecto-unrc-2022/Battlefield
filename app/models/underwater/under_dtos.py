e4d00a4dbc9291576cf05d991963e6115ecaeb7b
Joel D'Autilio <joeldautilio@gmail.com>
2022-09-27 19:34:41 -0300

    Add submarine and torpedo schema

--- app/models/underwater/under_dtos.py
+++ app/models/underwater/under_dtos.py
@@ -14,7 +14,42 @@ class UnderGameSchema(SQLAlchemyAutoSchema):
     host_id = auto_field()
     visitor_id = auto_field()
 
+    submarines = Nested(SubmarineSchema, many=True)
+    torpedos = Nested(TorpedoSchema, many=True)
 
-# class SubmarineSchema(SQLAlchemyAutoSchema):
-#     class Meta:
-#         model
+
+class SubmarineSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Submarine
+        include_relationships = True
+        load_instance = True
+
+    id = auto_field()
+    name = auto_field()
+    size = auto_field()
+    speed = auto_field()
+    visibility = auto_field()
+    radar_scope = auto_field()
+    health = auto_field()
+    torpedo_speed = auto_field()
+    torpedo_damage = auto_field()
+    x_position = auto_field()
+    y_position = auto_field()
+    direction = auto_field()
+    game_id = auto_field()
+    player_id = auto_field() 
+
+class TorpedoSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Submarine
+        include_relationships = True
+        load_instance = True
+
+    id = auto_field()
+    speed = auto_field()
+    damage = auto_field()
+    x_position = auto_field()
+    y_position = auto_field()
+    direction = auto_field()
+
+    game_id = auto_field()
