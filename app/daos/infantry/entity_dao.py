1c53ce21b9a12a7897413d6fc5813e167634e8c3
gaxel11 <axel_maicco@hotmail.com>
2022-09-29 23:03:32 -0300

    remove unnecessary code

--- app/daos/infantry/entity_dao.py
+++ app/daos/infantry/entity_dao.py
@@ -4,20 +4,6 @@ from ...models.user import User
 from ...models.infantry.entity import Entity
 from ...models.infantry.infantry_game import Game_Infantry
 
-def  add_entity(entity_id):
-    succes = True
-    if("1" == entity_id):
-        soldier = Entity(hp=10, velocidad=3)
-        db.session.add(soldier)
-        db.session.commit()
-    elif("2" == entity_id):
-        humvee = Entity(hp=20, velocidad=5)
-        db.session.add(humvee)
-        db.session.commit()
-    else:
-        succes = False
-    return succes
-
 #This method is used for the creating of game
 def create_game(user_id):
 
