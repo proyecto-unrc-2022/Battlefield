1a10fa0844a61014fb5cf6ada5ebf7a37cacbee0
AFungo <augustofungo@gmail.com>
2022-09-30 11:12:13 -0300

    Add scenarios for the users add a plane in the battlefield

--- features/steps/air_force_game.py
+++ features/steps/air_force_game.py
@@ -9,44 +9,41 @@ from app.models.airforce.air_force_game import AirForceGame
 from app.models.user import User
 
 
-@given("logged user")
+@given("three logged user")
 def step_impl(context):
-    user = User(username="Jhon", email="jhon@email", password="pass")
-    db.session.add(user)
+    user_a = User(username="Jhon", email="jhon@email", password="pass")
+    db.session.add(user_a)
+    user_b = User(username="Peter", email="Peter@email", password="pass")
+    db.session.add(user_b)
+    user_c = User(username="Vladimir", email="vodka@email", password="pass")
+    db.session.add(user_c)
     db.session.commit()
-    context.player = user.id
+
+    context.player_a = user_a.id
+    context.player_b = user_b.id
+    context.player_c = user_c.id
 
 
 @when("enter in empty game")
 def step_impl(context):
     context.response = context.client.put(
-        url_for("air_force.join_in_game", player=context.player)
+        url_for("air_force.join_in_game", player=context.player_a)
     )
     assert context.response.status_code is 200
 
 
 @then("players id who are in the game are returned")
 def step_impl(context):
-    raw_expected = {"player_a": str(context.player), "player_b": None}
+    raw_expected = {"player_a": str(context.player_a), "player_b": None}
     raw_response = context.response.json
 
     response, expected = json.dumps(raw_expected, sort_keys=True), json.dumps(
         raw_response, sort_keys=True
     )
-    print(response)
-    print(expected)
     assert response == expected
 
 
-@given("a second user")
-def step_impl(context):
-    user = User(username="Peter", email="Peter@email", password="pass")
-    db.session.add(user)
-    db.session.commit()
-    context.player_b = user.id
-
-
-@when("new user enter in the game")
+@when("second user enter in the game")
 def step_impl(context):
     context.response = context.client.put(
         url_for("air_force.join_in_game", player=context.player_b)
@@ -57,40 +54,35 @@ def step_impl(context):
 @then("two users info are returned")
 def step_impl(context):
     raw_expected = {
-        "player_a": str(1),  # esta croto esto ver mejor ;)
+        "player_a": str(context.player_a),
         "player_b": str(context.player_b),
     }
     raw_response = context.response.json
     response, expected = json.dumps(raw_expected, sort_keys=True), json.dumps(
         raw_response, sort_keys=True
     )
+    print(response)
+    print(expected)
+
     assert response == expected
-
-
-@given("a third user")
-def step_impl(context):
-    user = User(username="Vladimir", email="vodka@email", password="pass")
-    db.session.add(user)
-    db.session.commit()
-    context.player_b = user.id
 
 
 @when("new user try enter in the game")
 def step_impl(context):
     context.response = context.client.put(
-        url_for("air_force.join_in_game", player=context.player_b)
+        url_for("air_force.join_in_game", player=context.player_c)
     )
 
 
-@then("exception are returned")
+@then("status code 400 is returned")
 def step_impl(context):
     print(context.response.status_code)
     assert context.response.status_code == 400
 
 
-@given("a user in the game and plane in db")
+@given("player_a and plane in db")
 def step_impl(context):
-    context.player = AirForceGame.player_a
+    context.player_a = AirForceGame.player_a
     context.plane = add_plane(
         name="Hawker Tempest",
         size=1,
@@ -104,10 +96,11 @@ def step_impl(context):
 
 @when("choose a plane and his position")
 def step_impl(context):
+    print(context.player_a)
     context.response = context.client.put(
         url_for(
             "air_force.choise_plane_and_position",
-            player=context.player,
+            player=context.player_a,
             plane=context.plane.id,
             x=3,
             y=3,
@@ -119,3 +112,66 @@ def step_impl(context):
 @then("201 response are returned")
 def step_impl(context):
     assert context.response.status_code == 201
+
+
+@when("choose a plane and position outside of map")
+def step_impl(context):
+    context.response = context.client.put(
+        url_for(
+            "air_force.choise_plane_and_position",
+            player=context.player_a,
+            plane=context.plane.id,
+            x=21,
+            y=11,
+            course=2,
+        )
+    )
+
+
+@when("choose a plane and position in player_b position")
+def step_impl(context):
+    context.response = context.client.put(
+        url_for(
+            "air_force.choise_plane_and_position",
+            player=context.player_a,
+            plane=context.plane.id,
+            x=12,
+            y=5,
+            course=2,
+        )
+    )
+
+
+@then("Error status code are returned")
+def step_impl(context):
+    print(context.response.status_code)
+    assert context.response.status_code == 400
+
+
+@given("player_b in the game and plane in db")
+def step_impl(context):
+    context.player_b = AirForceGame.player_b
+    context.plane = add_plane(
+        name="Hawker Tempest",
+        size=1,
+        speed=5,
+        health=10,
+        course=3,
+        coor_x=5,
+        coor_y=5,
+    )
+
+
+@when("choose a plane and position in player_a position")
+def step_impl(context):
+    print(context.player_b)
+    context.response = context.client.put(
+        url_for(
+            "air_force.choise_plane_and_position",
+            player=context.player_b,
+            plane=context.plane.id,
+            x=1,
+            y=5,
+            course=2,
+        )
+    )
