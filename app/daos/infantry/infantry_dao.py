12affe4439024bb3beb19ce5e3966dee4d06375f
gaxel11 <axel_maicco@hotmail.com>
2022-10-01 19:09:07 -0300

    Agrego el metodo move

--- app/daos/infantry/infantry_dao.py
+++ app/daos/infantry/infantry_dao.py
@@ -1,7 +1,16 @@
+from turtle import left, right
 from app import db
 from app.models.user import Profile
 from ...models.infantry.infantry_game import Figure_infantry
 from ...models.infantry.infantry_game import Game_Infantry
+
+EAST = 1
+SOUTH_EAST = 2
+SOUTH = 3
+SOUTH_WEST = 4
+WEST = 5
+NORTH_WEST = 6
+NORTH = 7
 
 def  add_entity(entity_id):
     succes = True
@@ -23,4 +32,44 @@ def  add_entity(entity_id):
         db.session.commit()
     else:
         succes = False
+    return succes
+
+def move(user_id, direction, velocity):
+    figure = Figure_infantry.query.filter_by(id_user = user_id).first()
+    exceeded_velocity_limit = velocity <= figure.velocidad
+    succes = True
+    if(direction == EAST and exceeded_velocity_limit):
+        figure.direccion = EAST
+        figure.pos_x = figure.pos_x + velocity
+        db.session.commit()
+    elif(direction == SOUTH_EAST and exceeded_velocity_limit):
+        figure.direccion = SOUTH_EAST
+        figure.pos_x = figure.pos_x + velocity
+        figure.pos_y = figure.pos_y - velocity
+        db.session.commit()
+    elif(direction == SOUTH and exceeded_velocity_limit):
+        figure.direccion = SOUTH
+        figure.pos_y = figure.pos_y - velocity
+        db.session.commit()
+    elif(direction == SOUTH_WEST and exceeded_velocity_limit):
+        figure.direccion = SOUTH_WEST
+        figure.pos_x = figure.pos_x - velocity
+        figure.pos_y = figure.pos_y - velocity
+        db.session.commit()
+    elif(direction == WEST and exceeded_velocity_limit):
+        figure.direccion = WEST
+        figure.pos_x = figure.pos_x - velocity
+        db.session.commit()
+    elif(direction == NORTH_WEST and exceeded_velocity_limit):
+        figure.direccion = NORTH_WEST
+        figure.pos_x = figure.pos_x - velocity
+        figure.pos_y = figure.pos_y + velocity
+        db.session.commit()
+    elif(direction == NORTH and exceeded_velocity_limit):
+        figure.direccion = NORTH
+        figure.pos_x = figure.pos_x - velocity
+        figure.pos_y = figure.pos_y + velocity
+        db.session.commit()
+    else:
+        succes = False;
     return succes
