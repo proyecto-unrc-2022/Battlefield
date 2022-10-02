f8536772b3fe5be9fc1aa889ab47ca679976ac30
Nacho521 <juanignacioromero123456@gmail.com>
2022-10-02 18:10:13 -0300

    Remove methods

--- app/daos/infantry/infantry_dao.py
+++ app/daos/infantry/infantry_dao.py
@@ -73,3 +73,32 @@ def move(user_id, direction, velocity):
     else:
         succes = False;
     return succes
+
+#This method is used for the creating of game
+def create_game(user_id):
+
+    game = Game_Infantry(id_user1= user_id, id_user2= None)
+    db.session.add(game)
+    db.session.commit()
+    return db.session.query(Game_Infantry).order_by(Game_Infantry.id.desc()).first().id  
+
+    #if(db.session.query(Game_Infantry).first() != None):
+    #    return True
+    #else:
+    #    return False
+
+#This method is used to choose the order of the players
+#def start_of_game(user_id):
+    
+    #if(db.session.query(Game_Infantry).first().id_user1 == None):
+    #    Game_Infantry.id_user1 = user_id
+    #elif(db.session.query(Game_Infantry).first().id_user2 == None):
+    #    Game_Infantry.id_user2 = user_id       
+    
+#This method is used to ask if both players are in the game
+def ready(game_id):
+
+    if(db.session.query(Game_Infantry).get(1).id_user1 == None or db.session.query(Game_Infantry).get(1).id_user2 == None):
+        return False
+    else:
+        return True
