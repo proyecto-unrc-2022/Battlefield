0795f7bbc62b98f5c994cd817f418a5f450af979
LucioMansilla <luciomansillaztw@gmail.com>
2022-10-02 23:49:01 -0300

    Update missiles
    Co-authored-by: mateovieyra <mateovieyra@users.noreply.github.com>
    Co-authored-by: BrendaDichiara <BrendaDichiara@users.noreply.github.com>
    Co-authored-by: ValenButtignol <ValenButtignol@users.noreply.github.com>

--- app/navy/navy_constants.py
+++ app/navy/navy_constants.py
@@ -3,4 +3,28 @@ PATH_TO_START = "app/navy/start.json"
 ROWS = 10
 COLS = 20
 
+BORDERS ={
+    "top": 1,
+    "bottom": ROWS,
+    "left": 1,
+    "right": COLS,
+}
+
+
+
 MINIMUM_HP = 0
+
+XCORD = 0
+YCORD = 1
+
+COORDS = {
+    "N": (-1, 0),
+    "S": (1, 0),
+    "E": (0, 1),
+    "W": (0, -1),
+    "NE": (-1, 1),
+    "NW": (-1, -1),
+    "SE": (1, 1),
+    "SW": (1, -1),
+}
+
