de839d2da558a49d73ccb5b8ec7b3249c8018638
Nacho521 <juanignacioromero123456@gmail.com>
2022-10-03 00:44:38 -0300

    Add views for join the second player

--- api/v1/infantry/views.py
+++ api/v1/infantry/views.py
@@ -6,8 +6,9 @@ from flask import Response
 
 from app.daos.infantry.infantry_dao import add_entity
 from app.daos.infantry.infantry_dao import move
-from app.daos.infantry.entity_dao import create_game
-from app.daos.infantry.entity_dao import ready
+from app.daos.infantry.infantry_dao import create_game
+from app.daos.infantry.infantry_dao import ready
+from app.daos.infantry.infantry_dao import join
 
 from . import infantry
 
@@ -25,6 +26,13 @@ def ready_to_play(game_id):
         return Response(status=200)
     else:
         return Response(status=404)
+
+@infantry.route("/start/<game_id>/<user_id>",methods=['POST'])
+def join_game(game_id, user_id):
+    if (join(game_id, user_id)):
+        return Response(status=200)
+    else:
+        return Response(status=404)        
 
 @infantry.route("/<entity_id>",methods=['POST'])
 def choose_entity(entity_id):
