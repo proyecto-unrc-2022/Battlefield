a48e23ccfdd5cb3580a4bddbde43d3924398cd8a
gaxel11 <axel_maicco@hotmail.com>
2022-10-05 01:41:27 -0300

    Test para el metodo move_by_user

--- features/steps/infantry_step.py
+++ features/steps/infantry_step.py
@@ -2,95 +2,13 @@ from app import db
 from app.models.infantry.infantry_game import Figure_infantry, Game_Infantry
 from app.models.user import Profile
 from flask import url_for
+from app.daos.infantry.direction import *
 
 from app.models.user import User
 from app.daos.user_dao import add_user
 
-EAST = 1
-SOUTH_EAST = 2
-SOUTH = 3
-SOUTH_WEST = 4
-WEST = 5
-NORTH_WEST = 6
-NORTH = 7
 
-@given('a user Ignacio')
-def step_impl(context):
-    user = User(username= "Ignacio", email="Ignacio@gmail.com", password="123")
-    add_user(user.username, user.password, user.email)
-    assert True
-
-@when('you choose your soldier')
-def step_impl(context):
-    context.page = context.client.post(url_for("infantry.choose_entity",entity_id= 1))
-    assert context.page.status_code == 200
-    
-@then('the soldier is created')
-def step_impl(context):
-    ob = Figure_infantry.query.order_by(Figure_infantry.id.desc()).first()
-    context.ob = ob
-    is_created = context.ob.id == 1
-    is_soldier = Figure_infantry.query.filter_by(id=1).first().figure_type == 1
-    assert is_created and is_soldier
-
-@given('a user Matias')
-def step_impl(context):
-    user = User(username= "Matias", email="Matias@gmail.com", password="123")
-    add_user(user.username, user.password, user.email)
-    assert True
-
-@when('you choose your Humvee')
-def step_impl(context):
-    context.page = context.client.post(url_for("infantry.choose_entity",entity_id= 2))
-    assert context.page.status_code == 200
-    
-@then('the humvee is created')
-def step_impl(context):
-    ob = Figure_infantry.query.order_by(Figure_infantry.id.desc()).first()
-    context.ob = ob
-    is_created = context.ob.id == 2
-    is_humvee = Figure_infantry.query.filter_by(id=2).first().figure_type == 2
-    assert is_created and is_humvee
-
-@given('a user Lucas')
-def step_given(context) :
-    user = User(username= "Lucas", email="Lucas@gmail.com", password="1234")
-    add_user(user.username, user.password, user.email)
-    assert True   
-    
-@when('you choose your tank')
-def step_when(context) :
-    context.page = context.client.post(url_for("infantry.choose_entity",entity_id= 3))
-    assert context.page.status_code == 200
-    
-@then('the tank is created')
-def step_then(context) :
-    ob = Figure_infantry.query.order_by(Figure_infantry.id.desc()).first()
-    context.ob = ob
-    is_created = context.ob.id == 3
-    is_tank = Figure_infantry.query.filter_by(id=3).first().figure_type == 3
-    assert is_created and is_tank
-  
-@given('a user Ricardo')
-def step_given(context) :
-    user = User(username= "Ricardo", email="Ricardo@gmail.com", password="1324")
-    add_user(user.username, user.password, user.email)
-    assert True   
-
-@when('you choose your artillery')
-def step_when(context) :
-    context.page = context.client.post(url_for("infantry.choose_entity",entity_id=4))
-    assert context.page.status_code == 200 
-
-@then('the artillery is created')
-def step_then(context) :
-    ob = Figure_infantry.query.order_by(Figure_infantry.id.desc()).first()
-    context.ob = ob
-    is_created = context.ob.id == 4
-    is_artillery = Figure_infantry.query.filter_by(id=4).first().figure_type == 4
-    assert is_created and is_artillery
-
-@given('the first player')
+@given('the first player Franco')
 def step_impl(context):
     user1 = User(username= "Franco", email="Franco@gmail.com", password="123")
     add_user(user1.username, user1.password, user1.email)
@@ -102,19 +20,19 @@ def step_impl(context):
     context.page = context.client.post(url_for("infantry.start_game",user_id= 1))
     assert context.page.status_code == 200
 
-@then(u'it is the first player')
+@then('it is the first player')
 def step_impl(context):
     context.page = context.client.post(url_for("infantry.ready_to_play",game_id= 1))
     assert context.page.status_code == 404
 
-@given(u'the second player')
+@given('the second player Tomas')
 def step_impl(context):
     user2 = User(username= "Tomas", email="Tomas@gmail.com", password="123")
     add_user(user2.username, user2.password, user2.email)
     context.user = user2
     assert True
 
-@when(u'he joins the game')
+@when('he joins the game')
 def step_impl(context):
     game = Game_Infantry(id_user1= 1, id_user2= None)
     db.session.add(game)
@@ -122,22 +40,99 @@ def step_impl(context):
     context.page = context.client.post(url_for("infantry.join_game",game_id= 1, user_id= 2))
     assert context.page.status_code == 200
 
-@then(u'he is the second player')
+@then('he is the second player')
 def step_impl(context):
     game = Game_Infantry.query.filter_by(id = 1).first()
     assert game.id_user2 == 2
+    
+@given('a user Franco')
+def step_impl(context):
+    user = User.query.filter_by(username = "Franco").first()
+    assert not(user == None)
 
-#@given('un usuario Ignacio')
-#def step_given(context) :
-#    user = User.query.filter_by(username = "Ignacio").first()
-#    assert user is not None
+@when('you choose your soldier')
+def step_impl(context):
+    context.page = context.client.post(url_for("infantry.choose_entity", game_id = 1, user_id = 1, entity_id= 1))
+    assert context.page.status_code == 200
+    
+@then('the soldier is created')
+def step_impl(context):
+    ob = Figure_infantry.query.filter_by(id_user = 1, id_game = 1).first()
+    context.ob = ob
+    is_created = context.ob.id == 1
+    is_soldier = context.ob.figure_type == 1
+    assert is_created and is_soldier
 
-#@when('elige moverse hacia el este')
-#def step_when(context) :
-#    context.page = context.client.post(url_for("infantry.mov_action", user_id = 1, direction = EAST))
+@given('a user Tomas')
+def step_impl(context):
+    user = user = User.query.filter_by(username = "Tomas").first()
+    assert not(user == None)
 
-#@then('entonces la unidad se mueve hacia el este')
-#def step_then() :
-#    assert True
-#    # This was autogenerated using cucumber syntax.
-#    # Please convert to use regular expressions, as Behave does not currently support Cucumber Expressions   
+@when('you choose your Humvee')
+def step_impl(context):
+    context.page = context.client.post(url_for("infantry.choose_entity", game_id = 1, user_id = 2, entity_id= 2))
+    assert context.page.status_code == 200
+    
+@then('the humvee is created')
+def step_impl(context):
+    ob = Figure_infantry.query.filter_by(id_user = 2, id_game = 1).first()
+    context.ob = ob
+    is_created = context.ob.id == 2
+    is_humvee = context.ob.figure_type == 2
+    assert is_created and is_humvee
+
+# @given('a user Lucas')
+# def step_given(context) :
+#     user = User(username= "Lucas", email="Lucas@gmail.com", password="1234")
+#     add_user(user.username, user.password, user.email)
+#     assert True   
+    
+# @when('you choose your tank')
+# def step_when(context) :
+#     context.page = context.client.post(url_for("infantry.choose_entity",entity_id= 3))
+#     assert context.page.status_code == 200
+    
+# @then('the tank is created')
+# def step_then(context) :
+#     ob = Figure_infantry.query.order_by(Figure_infantry.id.desc()).first()
+#     context.ob = ob
+#     is_created = context.ob.id == 3
+#     is_tank = Figure_infantry.query.filter_by(id=3).first().figure_type == 3
+#     assert is_created and is_tank
+  
+# @given('a user Ricardo')
+# def step_given(context) :
+#     user = User(username= "Ricardo", email="Ricardo@gmail.com", password="1324")
+#     add_user(user.username, user.password, user.email)
+#     assert True   
+
+# @when('you choose your artillery')
+# def step_when(context) :
+#     context.page = context.client.post(url_for("infantry.choose_entity",entity_id=4))
+#     assert context.page.status_code == 200 
+
+# @then('the artillery is created')
+# def step_then(context) :
+#     ob = Figure_infantry.query.order_by(Figure_infantry.id.desc()).first()
+#     context.ob = ob
+#     is_created = context.ob.id == 4
+#     is_artillery = Figure_infantry.query.filter_by(id=4).first().figure_type == 4
+#     assert is_created and is_artillery
+
+
+
+@given('un usuario Franco')
+def step_given(context) :
+    user = User.query.filter_by(username = "Franco").first()
+    assert not(user == None)
+
+@when('elige mover su unidad hacia el este')
+def step_when(context) :
+    context.page = context.client.post(url_for("infantry.mov_action", game_id = 1, user_id = 1, velocity = 1, direction = EAST))
+    assert context.page.status_code == 200
+    
+
+@then('entonces la unidad se mueve hacia el este')
+def step_then(context) :
+    figure = Figure_infantry.query.filter_by(id_game = 1, id_user = 1).first()
+    assert figure.pos_x == 1
