70169e160e565dc936b134120df93fe2d4e3ca63
Nacho521 <juanignacioromero123456@gmail.com>
2022-10-06 10:37:03 -0300

    Views modified

--- api/v1/infantry/views.py
+++ api/v1/infantry/views.py
@@ -4,7 +4,7 @@ from app import db
 
 from flask import Response, request
 
-from app.daos.infantry.infantry_dao import add_entity
+from app.daos.infantry.infantry_dao import add_figure
 from app.daos.infantry.infantry_dao import create_game
 from app.daos.infantry.infantry_dao import ready
 from app.daos.infantry.infantry_dao import join
@@ -14,7 +14,7 @@ from app.daos.infantry.infantry_dao import shoot
 
 from . import infantry
 
-@infantry.route("/game/<user_id>",methods=['POST'])
+@infantry.route("/game/user/<user_id>",methods=['POST'])
 def start_game(user_id):
 
     if(create_game(user_id) != None):
@@ -23,23 +23,23 @@ def start_game(user_id):
         return Response(status=404)
     
 
-@infantry.route("/start/<game_id>",methods=['POST'])
+@infantry.route("/start/game/<game_id>",methods=['POST'])
 def ready_to_play(game_id):
     if (ready(game_id)):
         return Response(status=200)
     else:
         return Response(status=404)
 
-@infantry.route("/start/<game_id>/<user_id>",methods=['POST'])
+@infantry.route("/start/game/<game_id>/user/<user_id>",methods=['POST'])
 def join_game(game_id, user_id):
     if (join(game_id, user_id)):
         return Response(status=200)
     else:
         return Response(status=404)        
 
-@infantry.route("/create_entity/<game_id>/<user_id>/<entity_id>",methods=['POST'])
-def choose_entity(game_id, user_id ,entity_id):
-    if (add_entity(game_id, user_id ,entity_id)):
+@infantry.route("/create_entity/game/<game_id>/user/<user_id>/figure/<figure_id>",methods=['POST'])
+def choose_figure(game_id, user_id ,figure_id):
+    if (add_figure(game_id, user_id ,figure_id)):
         return Response(status=200)
     else:
         return Response(status=404)
