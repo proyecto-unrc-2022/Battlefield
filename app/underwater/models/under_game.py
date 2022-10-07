9c8bc80c1bd758edda27cbb2b5e668ae2b53da2a
PabloRoss7 <pablorossi2001@gmail.com>
2022-10-07 19:35:03 -0300

    Split bussiness methods from dao's methods
    Co-authored-by: Jonathan Rojo <JoniRojo@users.noreply.github.com>
    Co-authored-by: Joel D'Autilio <Jota98daut@users.noreply.github.com>

--- app/underwater/models/under_game.py
+++ app/underwater/models/under_game.py
@@ -1,6 +1,17 @@
+import json
+
+from sqlalchemy.orm import relationship
+
 from app import db
 from app.models.user import User
-from sqlalchemy.orm import relationship
+from app.underwater import boards
+from app.underwater.models.submarine import Submarine
+from app.underwater.models.submerged_object import SubmergedObject
+from app.underwater.models.torpedo import Torpedo
+from app.underwater.under_board import UnderBoard
+
+from ..daos.submarine_dao import SubmarineDAO
+
 
 class UnderGame(db.Model):
     __tablename__ = "under_game"
@@ -9,10 +20,100 @@ class UnderGame(db.Model):
     host_id = db.Column(db.Integer, db.ForeignKey(User.id))
     visitor_id = db.Column(db.Integer, db.ForeignKey(User.id))
 
-    host = relationship("User", backref="under_game_h", foreign_keys=[host_id])
-    visitor = relationship("User", backref="under_game_v", foreign_keys=[visitor_id])
+    host = relationship("User", backref="under_game_host", foreign_keys=[host_id])
+    visitor = relationship(
+        "User", backref="under_game_visitor", foreign_keys=[visitor_id]
+    )
 
-    submerged_objects= relationship("SubmergedObject", back_populates="game")
-    
+    submerged_objects = relationship("SubmergedObject", back_populates="game")
+
+    def __init__(self, host_id, height=10, width=20):
+        self.board = UnderBoard(height, width)
+        self.host_id = host_id
+        boards.update({self.id: self.board})
+
     # submarines = relationship("Submarine", back_populates="game")
     # torpedos = relationship("Torpedo", back_populates="game")
+
+    @staticmethod
+    def get_options():
+        options_json = open("app/underwater/options.json")
+        options = json.load(options_json)
+        return options
+
+    @staticmethod
+    def get_submarine_option(option_id):
+        options = UnderGame.get_options()
+
+        try:
+            choosen = options[str(option_id)]
+        except Exception:
+            return None
+
+        return choosen
+
+    def has_user(self, player_id):
+        return self.host_id == player_id or self.visitor_id == player_id
+
+    def add_submarine(self, player_id, option_id, x_coord, y_coord, direction):
+        sub_stats = UnderGame.get_submarine_option(option_id)
+
+        if not self.has_user(player_id):
+            raise ValueError("the game does not have the specified player")
+
+        for obj in self.submerged_objects:
+            if obj.player_id == player_id:
+                raise Exception("Player already has a submarine")
+
+        sub = SubmarineDAO.create_submarine(self.id, player_id, sub_stats)
+        self.submerged_objects.append(sub)
+        self.place(sub, x_coord, y_coord, direction)
+        return sub
+
+    def place(self, obj, x_coord, y_coord, direction):
+        if obj.is_placed():
+            raise Exception("submarine is already placed")
+
+        obj.set_position(x_coord, y_coord, direction)
+        self.board.place(obj)
+
+    def contains_object(self, obj_id):
+        for obj in self.submerged_objects:
+            if obj.id == obj_id:
+                return True
+        return False
+
+    def get_id(self):
+        return self.id
+
+    def get_host(self):
+        return self.host
+
+    def get_visitor(self):
+        return self.visitor
+
+    def get_submerged_objects(self, model=SubmergedObject):
+        ret_list = []
+        for obj in self.submerged_objects:
+            if isinstance(obj, model):
+                ret_list.append(obj)
+        return ret_list
+
+    def get_submarines(self):
+        return self.get_submerged_objects(Submarine)
+
+    def get_torpedos(self):
+        return self.get_submerged_objects(Torpedo)
+
+    def rotate_object(self, obj, direction):
+        if direction == obj.direction:
+            return
+
+        new_cells = obj.get_tail_positions(direction=direction)
+        for (x, y) in new_cells:
+            if self.board.matrix[x][y]:
+                self.run_conflict(obj, self.board.matrix[x][y])
+
+        self.board.clear(obj.get_positions())
+        obj.set_position(direction=direction)
+        self.board.place(obj)
