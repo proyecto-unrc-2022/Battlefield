9046e0f331a11a190e3596b2b60a7092ddd2533e
mateovieyra <mateovieyra8@gmail.com>
2022-10-10 18:34:55 -0300

    Add NavyGame DTO

---
+++ app/navy/dtos/navy_game_dto.py
@@ -0,0 +1,20 @@
+from marshmallow_sqlalchemy import SQLAlchemySchema, auto_field
+from marshmallow_sqlalchemy.fields import Nested
+from app.navy.models.navy_game import NavyGame
+from app.navy.dtos.missile_dto import MissileDTO
+from app.navy.dtos.ship_dto import ShipDTO
+
+class NavyGameDTO(SQLAlchemySchema):
+  class Meta:
+    model = NavyGame
+    include_relationships = True
+    load_instances = True
+
+  id = auto_field()
+  board_rows = auto_field()
+  board_colums = auto_field()
+  winner = auto_field()
+  round = auto_field()
+  turn = auto_field()              
+  ships = Nested(ShipDTO, many=True)
+  missiles = Nested(MissileDTO, many=True)
