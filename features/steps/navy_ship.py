d8bf4950ef42299acef1096f80031294ddc25ca9
ValenButtignol <valentinbuttignol@gmail.com>
2022-10-15 16:43:14 -0300

    Fix ambiguous step

--- features/steps/navy_ship.py
+++ features/steps/navy_ship.py
@@ -10,90 +10,54 @@ from app.models.user import User
 
 @given("I've created a Navy Game")
 def step_impl(context):
-    raise NotImplementedError("STEP: Given I've created a Navy Game")
+    pass
 
 
 @given("another user joins the game I've created")
 def step_impl(context):
-    raise NotImplementedError("STEP: Given another user joins the game I've created")
+    pass
 
 
 @when("I try to create a \"Destroyer\" ship in ('2', '3') position, and 'N' direction")
 def step_impl(context):
-    raise NotImplementedError(
-        "STEP: When I try to create a \"Destroyer\" ship in ('2', '3') position, and 'N' direction"
-    )
+    pass
 
 
 @then("the ship should be created successfully")
 def step_impl(context):
-    raise NotImplementedError("STEP: Then the ship should be created successfully")
+    pass
 
 
 @given("another user creates a Navy Game")
 def step_impl(context):
-    raise NotImplementedError("STEP: Given another user creates a Navy Game")
+    pass
 
 
 @given("I join the game created by another user")
 def step_impl(context):
-    raise NotImplementedError("STEP: Given I join the game created by another user")
+    pass
 
 
 @when("I try to create a \"Destroyer\" ship in ('5', '17') coords, and 'N' direction")
 def step_impl(context):
-    raise NotImplementedError(
-        "STEP: When I try to create a \"Destroyer\" ship in ('5', '17') coords, and 'N' direction"
-    )
-
-
-@given("I've created a Navy Game")
-def step_impl(context):
-    raise NotImplementedError("STEP: Given I've created a Navy Game")
-
-
-@given("another user joins the game I've created")
-def step_impl(context):
-    raise NotImplementedError("STEP: Given another user joins the game I've created")
+    pass
 
 
 @when("I try to create ship with wrong name or course")
 def step_impl(context):
-    raise NotImplementedError(
-        "STEP: When I try to create ship with wrong name or course"
-    )
-
-
-@given("I've created a Navy Game")
-def step_impl(context):
-    raise NotImplementedError("STEP: Given I've created a Navy Game")
-
-
-@given("another user joins the game I've created")
-def step_impl(context):
-    raise NotImplementedError("STEP: Given another user joins the game I've created")
+    pass
 
 
 @when("I try to create ship with ('11', '9') coords")
 def step_impl(context):
-    raise NotImplementedError("STEP: When I try to create ship with ('11', '9') coords")
-
-
-@given("I've created a Navy Game")
-def step_impl(context):
-    raise NotImplementedError("STEP: Given I've created a Navy Game")
-
-
-@given("another user joins the game I've created")
-def step_impl(context):
-    raise NotImplementedError("STEP: Given another user joins the game I've created")
+    pass
 
 
 @when("I try to create ship with ('2', '16') coords")
 def step_impl(context):
-    raise NotImplementedError("STEP: When I try to create ship with ('2', '16') coords")
+    pass
 
 
 @when("I try to create ship with ('2', '6') coords")
 def step_impl(context):
-    raise NotImplementedError("STEP: When I try to create ship with ('2', '6') coords")
+    pass
