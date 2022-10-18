412c8f2f5e913c85c656936636cf530c533125d2
Nacho521 <juanignacioromero123456@gmail.com>
2022-10-18 13:33:10 -0300

    Add views for the update of projectile

--- api/v1/infantry/views.py
+++ api/v1/infantry/views.py
@@ -11,6 +11,7 @@ from app.daos.infantry.infantry_dao import ready
 from app.daos.infantry.infantry_dao import join
 from app.daos.infantry.infantry_dao import move_by_user
 from app.daos.infantry.infantry_dao import shoot
+from app.daos.infantry.infantry_dao import update_projectile
 from app.models.infantry.infantry_game import Game_Infantry, Figure_infantry, Game_Infantry_Schema, Projectile_Infantry_Schema, Figure_Infantry_Schema, Projectile
 from . import infantry
 
@@ -27,7 +28,7 @@ def start_game(user_id):
     if(create_game(user_id) == None):
         return Response(status=404)
     
-    new_game = Game_Infantry.query.filter_by(id_user1 = user_id).first()
+    new_game = db.session.query(Game_Infantry).where(Game_Infantry.id_user1 == user_id).one_or_none()
 
     return jsonify(game_schema.dump(new_game))
     
@@ -51,7 +52,7 @@ def join_game(game_id, user_id):
 
     if (join(game_id, user_id)):
 
-        ready_game = Game_Infantry.query.filter_by(id = game_id, id_user2 = user_id).first()
+        ready_game = db.session.query(Game_Infantry).where(Game_Infantry.id == game_id and Game_Infantry.id_user2 == user_id).one_or_none()
 
         return jsonify(game_schema.dump(ready_game))
 
@@ -62,14 +63,13 @@ def join_game(game_id, user_id):
 
 @infantry.route("/create_entity/game/<game_id>/user/<user_id>/figure/<figure_id>",methods=['POST'])
 def choose_figure(game_id, user_id ,figure_id):
-    if (not(add_figure(game_id, user_id ,figure_id))):
+    if (add_figure(game_id, user_id ,figure_id)):
         
+        entity = db.session.query(Figure_infantry).where(Figure_infantry.id == game_id and Figure_infantry.id_user == user_id).one_or_none()
+
+        return jsonify(figure_schema.dump(entity))
+    else:
         return Response(status=404)
-
-    entity = Figure_infantry.query.filter_by(id_game = game_id, id_user = user_id).first()
-
-    return jsonify(figure_schema.dump(entity))
-        
 
 #revisar la logica de moverse 
 @infantry.route("/move/game/<game_id>/user/<user_id>/course/<direction>/velocity/<velocity>",methods=['POST'])
@@ -93,21 +93,17 @@ def shoot_entity(direction, figure_id, game_id):
     else:
         return Response(status=404)
 
+@infantry.route("/update",methods=['POST'])
+def update_projectile():
 
-#@infantry.route("/games",methods=['POST'])
-#def games_test():
-#
-#    data = json.loads(request.data)
-#    user1_id = data["id_user1"]
-#    user2_id = data["id_user2"]
-#
-#    game = Game_Infantry(id_user1= user1_id, id_user2= user2_id)
-#
-#    return jsonify(game_schema.dump(game))
-
+    projectile = db.session.query(Projectile).order_by(Projectile.id.desc()).first()
     
+    if(update_projectile(projectile)):
+    
+        return jsonify(projectile_schema.dump(projectile))
 
-
+    else:
+        return Response(status=404)
 
 
     
