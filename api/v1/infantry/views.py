a42ebb759f7e2dc9ae3ea3d8a7f6efec5e9e2894
gaxel11 <axel_maicco@hotmail.com>
2022-10-23 21:57:06 -0300

    json route for move and minor changes

--- api/v1/infantry/views.py
+++ api/v1/infantry/views.py
@@ -9,7 +9,7 @@ from app.daos.infantry.infantry_dao import add_figure
 from app.daos.infantry.infantry_dao import create_game
 from app.daos.infantry.infantry_dao import ready
 from app.daos.infantry.infantry_dao import join
-from app.daos.infantry.infantry_dao import move_by_user
+from app.daos.infantry.infantry_dao import move
 from app.daos.infantry.infantry_dao import shoot
 from app.daos.infantry.infantry_dao import update_projectile
 from app.models.infantry.infantry_game import Game_Infantry, Figure_infantry, Game_Infantry_Schema, Projectile_Infantry_Schema, Figure_Infantry_Schema, Projectile
@@ -64,30 +64,26 @@ def join_game(game_id, user_id):
 
 @infantry.route("/game/<game_id>/user/<user_id>/figure/<type>/create_entity",methods=['POST'])
 def choose_figure(game_id, user_id, type):
-
-    new_figure = add_figure(game_id, user_id, type)
-
+    new_figure = add_figure(game_id, user_id, int(type))
     if(new_figure == None):
-
         return Response(status=404)
-
-
     return jsonify(figure_schema.dump(new_figure))
 
 #revisar la logica de moverse 
-@infantry.route("/move/game/<game_id>/user/<user_id>/course/<direction>/velocity/<velocity>",methods=['POST'])
-def mov_action(direction, velocity, user_id, game_id):
-    if(move_by_user(game_id, user_id, direction, velocity)):
-        
+@infantry.route("/move",methods=['POST'])
+def mov_action():
+    data = json.loads(request.data)
+    velocity = data["velocity"]
+    course = data["course"]
+    game_id = data["game_id"]
+    user_id = data["user_id"]
+    if(move(int(game_id), int(user_id), int(course), int(velocity))):
         move_entity = db.session.query(Figure_infantry).where(Figure_infantry.id == game_id and Figure_infantry.id_user == user_id).one_or_none()
-        
         return jsonify(figure_schema.dump(move_entity))
-    
-    return Response(status=404)
+    return "Colision o velocidad excedida"
 
 @infantry.route("/shoot/game/<game_id>/course/<direction>/figure/<figure_id>",methods=['POST'])
 def shoot_entity(direction, figure_id, game_id):
-
     if(shoot(direction, figure_id, game_id)):
 
         projectile = db.session.query(Projectile).order_by(Projectile.id.desc()).first()
