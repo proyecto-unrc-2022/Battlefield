0488c41a0014a1f8520855208822d7218f70b85e
Nacho521 <juanignacioromero123456@gmail.com>
2022-11-01 18:43:10 -0300

    Add coords for body and projectile

--- app/daos/infantry/constant.py
+++ app/daos/infantry/constant.py
@@ -7,7 +7,7 @@ NORTH_WEST = 7
 NORTH = 0
 NORTH_EAST = 1
 
-COORDS = {
+COORDS_CUERPO = {
     0 : (0, -1), #N
     1 : (-1, -1), #NE
     2 : (-1, 0), #E
@@ -16,6 +16,17 @@ COORDS = {
     5 : (1, 1), #SO
     6 : (1, 0), #O
     7 : (1, -1), #NO
+}
+
+COORDS_MISIL = {
+    0 : (0, -1), #S
+    1 : (-1, -1), #SO
+    2 : (-1, 0), #0
+    3 : (-1, 1), #NO
+    4 : (0, 1), #N
+    5 : (1, 1), #NE
+    6 : (1, 0), #E
+    7 : (1, -1), #SE
 }
 
 MACHINE_GUN = 0
