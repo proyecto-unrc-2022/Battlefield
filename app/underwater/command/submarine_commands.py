e6c302bad5ff6b6bde580360df63ca69ac42ed7b
Joel D'Autilio <joeldautilio@gmail.com>
2022-11-01 22:15:39 -0300

    Make commands persistent

--- app/underwater/command/submarine_commands.py
+++ app/underwater/command/submarine_commands.py
@@ -1,9 +1,22 @@
+from sqlalchemy.orm import relationship
+
+from app import db
 from app.underwater.daos.submarine_dao import submarine_dao
 
 from .command import Command
 
 
 class SubmarineCommand(Command):
+    submarine_id = db.Column(db.Integer, db.ForeignKey("submarine.id"))
+    direction = db.Column(db.Integer)
+
+    submarine = relationship("Submarine")
+
+    __mapper_args__ = {
+        "polymorphic_identity": "submarine_command",
+        "polymorphic_on": Command.name,
+    }
+
     def __init__(self, game, submarine, **params):
         super(SubmarineCommand, self).__init__(game, submarine.player, **params)
         self.submarine = submarine
@@ -13,6 +26,17 @@ class SubmarineCommand(Command):
 
 
 class RotateAndAdvance(SubmarineCommand):
+    steps = db.Column(db.Integer)
+
+    __mapper_args__ = {
+        "polymorphic_identity": "rotate_and_advance",
+    }
+
+    def __init__(self, game, submarine, **params):
+        super().__init__(game, submarine, **params)
+        self.direction = params["direction"]
+        self.steps = params["steps"]
+
     def execute(self):
         submarine = self.submarine
         if submarine.in_game():
@@ -22,6 +46,14 @@ class RotateAndAdvance(SubmarineCommand):
 
 
 class RotateAndAttack(SubmarineCommand):
+    __mapper_args__ = {
+        "polymorphic_identity": "rotate_and_attack",
+    }
+
+    def __init__(self, game, submarine, **params):
+        super().__init__(game, submarine, **params)
+        self.direction = params["direction"]
+
     def execute(self):
         submarine = self.submarine
         if submarine.in_game():
--- app/underwater/command/torpedo_commands.py
+++ app/underwater/command/torpedo_commands.py
@@ -1,17 +1,31 @@
+from sqlalchemy.orm import relationship
+
+from app import db
+
 from ..daos.submerged_object_dao import submerged_object_dao
 from .command import Command
 
 
 class TorpedoCommand(Command):
+    torpedo_id = db.Column(db.Integer, db.ForeignKey("torpedo.id"))
+
+    torpedo = relationship("Torpedo")
+
+    __mapper_args__ = {
+        "polymorphic_identity": "torpedo_command",
+        "polymorphic_on": Command.name,
+    }
+
     def __init__(self, torpedo, **params):
         super(TorpedoCommand, self).__init__(torpedo.game, torpedo.player, **params)
         self.torpedo = torpedo
 
-    def get_torpedo(self):
-        return self.torpedo
-
 
 class AdvanceTorpedo(TorpedoCommand):
+    __mapper_args__ = {
+        "polymorphic_identity": "advance_torpedo",
+    }
+
     def execute(self):
         if self.torpedo.in_game():
             self.game.advance_object(self.torpedo)
