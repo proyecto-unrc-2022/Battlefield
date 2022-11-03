331c71ad21b34b7b723b2fa0b3ecb2915bde9051
Nacho521 <juanignacioromero123456@gmail.com>
2022-11-03 12:06:10 -0300

    Delete COORDS_MISIL

--- app/daos/infantry/constant.py
+++ app/daos/infantry/constant.py
@@ -18,17 +18,6 @@ COORDS_CUERPO = {
     7 : (1, -1), #NO
 }
 
-COORDS_MISIL = {
-    0 : (0, -1), #S
-    1 : (-1, -1), #SO
-    2 : (-1, 0), #0
-    3 : (-1, 1), #NO
-    4 : (0, 1), #N
-    5 : (1, 1), #NE
-    6 : (1, 0), #E
-    7 : (1, -1), #SE
-}
-
 MACHINE_GUN = 0
 MISSILE = 1
 MORTAR = 2
