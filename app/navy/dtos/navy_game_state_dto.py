910770fa101de12e896195b928d403c6feefd719
LucioMansilla <luciomansillaztw@gmail.com>
2022-11-02 23:51:57 -0300

    Change board -> shigt_range

--- app/navy/dtos/navy_game_state_dto.py
+++ app/navy/dtos/navy_game_state_dto.py
@@ -23,7 +23,7 @@ class NavyGameStateDTO:
         self.round = navy_game.round
         self.winner = navy_game.winner
         self.ship = self.load_ship(self.user_id)
-        self.board = navy_game_service.get_board(self.id, self.user_id)
+        self.shigt_range = navy_game_service.get_board(self.id, self.user_id)
 
     def dump(self):
         return {
@@ -36,5 +36,5 @@ class NavyGameStateDTO:
             "round": self.round,
             "winner": self.winner,
             "ship": self.ship,
-            "board": self.board,
+            "shigt_range": self.shigt_range,
         }
