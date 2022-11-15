fd3dcdd7a88f097ef414d7416a42ae6eb96d503e
joaquinmoran <joacomoran6@gmail.com>
2022-11-15 18:06:57 -0300

    delete AirforceLobby.components.js

--- client/src/airforce/AirforceAPP.js
+++ client/src/airforce/AirforceAPP.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import InitAirforce from "./components/InitAirforce.component";
-import AirforceLobby from "./components/AirforceLobby.component";
 import "./index.css"
 
 
@@ -20,7 +19,6 @@ class AirforceAPP extends Component {
     render() {
         return (
         <div>
-        
             <InitAirforce callBack={this.handleCallback}/> 
         </div>
     )}
