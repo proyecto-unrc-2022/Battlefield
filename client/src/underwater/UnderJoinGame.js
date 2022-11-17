6f4b18a3f210ce7ac014c1ab99a39b4758475392
Joel D'Autilio <joeldautilio@gmail.com>
2022-11-17 20:48:31 -0300

    Implement connection to api in Join button

--- client/src/underwater/UnderJoinGame.js
+++ client/src/underwater/UnderJoinGame.js
@@ -5,36 +5,45 @@ import authHeader from "../services/auth-header"
 
 const baseURL = "http://127.0.0.1:5000/api/v1/underwater";
 
-function SessionEntry({id, session}) {
+function SessionEntry(props) {
+  function sendJoinRequest(event) {
+    event.preventDefault();
+
+    axios.post(
+      baseURL + "/game/" + props.id + "/join",
+      {},
+      {headers: authHeader()}
+    ).then(response => {
+      ; 
+    }).catch(error => {
+      console.log(error.response.data);
+      props.setAlertMessage(error.response.data["error"]);
+    });
+  }
+
   return (
-    <li key={id}>
+    <li key={props.id}>
       <span>
-        id: {id}, 
-        host: {session.host_id}
+        id: {props.id}, 
+        host: {props.session.host_id}
       </span>
-      <button className="u-button u-small-button">Join</button>
+      <form onSubmit={sendJoinRequest}>
+        <button type="submit" className="u-button u-small-button">Join</button>
+      </form>
     </li>
     );
 }
 
-function GameList() {
-  const [sessions, setSessions] = useState({});
-
-  function updateSessionsList() {
-    axios.get(
-      baseURL + "/games",
-      {headers: authHeader()}
-    ).then(
-      (response) => { setSessions(response.data); }
-    );
-  }
-
-  useEffect(() => {updateSessionsList();}, []);
+function GameList({setAlertMessage, sessions, updateSessionsList}) {
+  useEffect(() => {
+    updateSessionsList();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <div className="row u-input-field">
       <ul className="u-list">
-        {Object.keys(sessions).map(key => {return <SessionEntry id={key} session={sessions[key]} />;})}
+        {Object.keys(sessions).map(key => {return <SessionEntry setAlertMessage={setAlertMessage} key={key} id={key} session={sessions[key]} />;})}
       </ul>
     </div>
     );
@@ -42,18 +51,29 @@ function GameList() {
 }
 
 export default function JoinGame(props) {
+  const [sessions, setSessions] = useState({});
+  const [alertMessage, setAlertMessage] = useState(null);
+
+  function updateSessionsList() {
+    axios.get(
+      baseURL + "/games",
+      {headers: authHeader()}
+    ).then(
+      (response) => { setSessions(response.data); }
+    ).catch((error) => {console.log(error);});
+  }
+
   return (
     <div className="u-small-container">
       <div className="row"><h3>Games</h3></div>
-      <form>
-        <div className="row">
-          <GameList />
-        </div>
-        <div className="row u-input-field">
-          <div onClick={() => {props.setVisibleComp("home")}} className="u-button">‹</div>
-          <button id="play-button" className="u-button">Update</button>
-        </div>
-      </form>
+      <div className="row">
+        <GameList setAlertMessage={setAlertMessage} sessions={sessions} updateSessionsList={updateSessionsList} />
+      </div>
+      <div className="row u-input-field">
+        <div onClick={() => {props.setVisibleComp("home")}} className="u-button">‹</div>
+        <div onClick={updateSessionsList} id="play-button" className="u-button">Update</div>
+      </div>
+      {alertMessage != null ? <span className="alert-danger">{alertMessage}</span> : null}
     </div>
     )
 }
