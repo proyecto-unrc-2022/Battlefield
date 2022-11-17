f165537607969f701350caec7d059dd44894a12a
Joel D'Autilio <joeldautilio@gmail.com>
2022-11-16 22:29:00 -0300

    Show games list in join game view

--- client/src/underwater/UnderJoinGame.js
+++ client/src/underwater/UnderJoinGame.js
@@ -5,40 +5,39 @@ import authHeader from "../services/auth-header"
 
 const baseURL = "http://127.0.0.1:5000/api/v1/underwater";
 
-function SessionEntry({session}) {
+function SessionEntry({id, session}) {
   return (
-    <li className="sesion-entry">
-      id: {session.id}, 
-      host: {session.host_id}
-      </li>
-  );
+    <li key={id}>
+      <span>
+        id: {id}, 
+        host: {session.host_id}
+      </span>
+      <button className="u-button u-small-button">Join</button>
+    </li>
+    );
 }
 
 function GameList() {
-  const [sessions, setSessions] = useState([]);
+  const [sessions, setSessions] = useState({});
 
-  useEffect(() => {
+  function updateSessionsList() {
     axios.get(
       baseURL + "/games",
       {headers: authHeader()}
     ).then(
-      (response) => {
-        setSessions([]);
-        console.log(response.data);
-        Object.keys(response.data).map(key => {console.log(response.data[key]); sessions.push(response.data[key])});
-      }
+      (response) => { setSessions(response.data); }
     );
-  }, []);
+  }
 
-  console.log(sessions);
+  useEffect(() => {updateSessionsList();}, []);
 
   return (
-    <>
-      <ul>
-        {sessions.map(session => {return <SessionEntry session={session} />;})}
+    <div className="row u-input-field">
+      <ul className="u-list">
+        {Object.keys(sessions).map(key => {return <SessionEntry id={key} session={sessions[key]} />;})}
       </ul>
-    </>
-  );
+    </div>
+    );
 
 }
 
@@ -47,9 +46,12 @@ export default function JoinGame(props) {
     <div className="u-small-container">
       <div className="row"><h3>Games</h3></div>
       <form>
-        <div className="row u-input-field">
+        <div className="row">
           <GameList />
+        </div>
+        <div className="row u-input-field">
           <div onClick={() => {props.setVisibleComp("home")}} className="u-button">‹</div>
+          <button id="play-button" className="u-button">Update</button>
         </div>
       </form>
     </div>
