427e88dc6bed530a50591742df80e7c3dcf24ed0
gaxel11 <axel_maicco@hotmail.com>
2022-11-17 04:07:35 -0300

    add useful methods

--- client/src/infantry/services/infantry.service.js
+++ client/src/infantry/services/infantry.service.js
@@ -4,21 +4,49 @@ import AuthService from "../../services/auth.service";
 const API_URL = "http://127.0.0.1:5000/api/v1/infantry/";
 class InfantryService{
 
-    getInfantry(user_id, game_id){
-        console.log(user_id)
-        let data = {game_id: game_id, user_id, user_id}
-        console.log(data)
-        return axios.post(
-            API_URL + "figure", 
-                data,
+    async getFigure(user_id, game_id){
+    let data = {game_id: game_id, user_id, user_id}
+    return await axios.post(
+        API_URL + "figure", 
+            data,
+        {
+            headers: {
+                "Content-Type": "application/json",
+                },
+        }
+    ).then((response) => {
+        return response.data;
+        });
+    }
+    async getProjectile(game_id){
+        let data = {game_id: game_id}
+        return await axios.post(
+            API_URL + "projectiles",
+            data,
             {
-                headers: {
+                headers:{
                     "Content-Type": "application/json",
-                  },
+                },
             }
-        ).then((response) => {
-            return response.data;
-          });
+        ).then((response) =>{
+            return response.data
+        });
+    }
+
+    async getUsers(game_id){
+        let data = {game_id: game_id}
+        return await axios.post(
+            API_URL + "game",
+            data,
+            {
+                headers:{
+                    "Content-Type": "application/json",
+                },
+            }
+        ).then((response) =>{
+            console.log(response.data)
+            return response.data
+        })
     }
 }
 
