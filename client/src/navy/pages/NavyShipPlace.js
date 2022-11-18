560f8400b3ba8c1377b8e156e393c2ea93e118d1
mateovieyra <mateovieyra8@gmail.com>
2022-11-18 17:48:14 -0300

    Protect Routes

--- client/src/navy/pages/NavyShipPlace.js
+++ client/src/navy/pages/NavyShipPlace.js
@@ -1,20 +1,43 @@
 import React, { useState, useEffect } from "react";
-import { useNavigate, useLocation } from "react-router-dom";
+import { useNavigate, useLocation, useParams } from "react-router-dom";
 import NavyButton from "../components/NavyButton";
 import ShipService from "../services/ShipService";
 import NavyGameService from "../services/NavyGameService";
+import authService from "../../services/auth.service";
+import AccessDenied from "../components/AccessDenied";
 
 export const NavyShipPlace = () => {
   const location = useLocation();
-  const [ship, setShip] = useState(location.state.ship_selected);
+  const [ship, setShip] = useState(location.state?.ship_selected);
   const [shipPlaced, setShipPlaced] = useState(false);
   const navigate = useNavigate();
+  const { id } = useParams();
+  const [accessDenied, setAccessDenied] = useState(true);
 
   useEffect(() => {
+    if (!location.state) {
+      navigate(`/navy/games/${id}/ship_selection`);
+    }
+
+    NavyGameService.getNavyGame(id).then((resp) => {
+      const currentUser = authService.getCurrentUser();
+      const accessDenied =
+        currentUser.sub !== resp.data.data.user_1.id &&
+        currentUser.sub !== resp.data.data.user_2.id;
+
+      setAccessDenied(accessDenied);
+      if (!accessDenied) {
+        if (resp.data.data.status === "STARTED") {
+          navigate(`/navy/games/${id}/board`);
+        } else if (resp.data.data.status === "WAITING_PLAYERS") {
+          navigate(`/navy/games/${id}/lobby`);
+        }
+      }
+    });
+
     if (shipPlaced) {
       const interval = setInterval(() => {
         NavyGameService.getNavyGame(ship.navy_game_id).then((resp) => {
-          console.log(resp.data);
           if (resp.data.data.status === "STARTED") {
             navigate(`/navy/games/${resp.data.data.id}/board`);
           }
@@ -44,63 +67,73 @@ export const NavyShipPlace = () => {
   };
 
   return (
-    <>
-      <h1>Ship: {ship.name}</h1>
-      <div className="row justify-content-center">
-        <div className="col-3 col-md-3 mx-auto">
-          <form>
-            <div className="form-group">
-              <label htmlFor="x">X</label>
-              <input
-                onChange={handleXCord}
-                type="number"
-                className="form-control"
-                id="x"
-                placeholder="X"
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="y">Y</label>
-              <input
-                onChange={handleYCord}
-                type="number"
-                className="form-control"
-                id="y"
-                placeholder="Y"
-              />
-            </div>
+    <div style={{ flexGrow: "1" }} className="container-fluid bg-navy">
+      {accessDenied ? (
+        <AccessDenied
+          text={"You can't join to this game"}
+          buttonText={"Go to games"}
+          redirectTo={"/navy/games"}
+        />
+      ) : (
+        <>
+          <h1>Ship: {ship?.name}</h1>
+          <div className="row justify-content-center">
+            <div className="col-3 col-md-3 mx-auto">
+              <form>
+                <div className="form-group">
+                  <label htmlFor="x">X</label>
+                  <input
+                    onChange={handleXCord}
+                    type="number"
+                    className="form-control"
+                    id="x"
+                    placeholder="X"
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="y">Y</label>
+                  <input
+                    onChange={handleYCord}
+                    type="number"
+                    className="form-control"
+                    id="y"
+                    placeholder="Y"
+                  />
+                </div>
 
-            <div className="form-group">
-              <label htmlFor="y">COURSE</label>
-              <input
-                onChange={handleCourse}
-                type="text"
-                className="form-control"
-                id="y"
-                placeholder="N"
-              />
+                <div className="form-group">
+                  <label htmlFor="y">COURSE</label>
+                  <input
+                    onChange={handleCourse}
+                    type="text"
+                    className="form-control"
+                    id="y"
+                    placeholder="N"
+                  />
+                </div>
+              </form>
             </div>
-          </form>
-        </div>
-      </div>
-      <div className="row justify-content-center">
-        {shipPlaced ? (
-          <div className="row d-flex flex-column justify-content-center align-items-center">
-            <div className="spinner-border m-3" role="status">
-              <span className="sr-only">Loading...</span>
-            </div>
-            <span className="text-center">
-              Waiting for the other player select his ship...
-            </span>
           </div>
-        ) : (
-          <NavyButton
-            action={placeShip}
-            text="Place and Play"
-            size={"medium"}
-          />
-        )}
-      </div>
-    </>
+          <div className="row justify-content-center">
+            {shipPlaced ? (
+              <div className="row d-flex flex-column justify-content-center align-items-center">
+                <div className="spinner-border m-3" role="status">
+                  <span className="sr-only">Loading...</span>
+                </div>
+                <span className="text-center">
+                  Waiting for the other player select his ship...
+                </span>
+              </div>
+            ) : (
+              <NavyButton
+                action={placeShip}
+                text="Place and Play"
+                size={"medium"}
+              />
+            )}
+          </div>
+        </>
+      )}
+    </div>
   );
 };
