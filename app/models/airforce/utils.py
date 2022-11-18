c6f2fc5f73009e3ef14d280511343b5a22441edf
AFungo <augustofungo@gmail.com>
2022-11-18 14:09:14 -0300

    create utils file with init_db method for airforce game

---
+++ app/models/airforce/utils.py
@@ -0,0 +1,21 @@
+import json
+
+from app.daos.airforce.plane_dao import add_plane, add_projectile
+from app.models.airforce.plane import Plane
+
+
+def init_db():
+    f = open("./app/models/airforce/planes.json")
+    data = json.load(f)
+    for plane in data["planes"]:
+        name = plane["name"]
+        size = plane["size"]
+        speed = plane["speed"]
+        health = plane["health"]
+        cant_projectile = plane["cant_projectile"]
+        add_plane(name, size, speed, health, cant_projectile)
+    for projectile in data["projectile"]:
+        speed = projectile["speed"]
+        damage = projectile["damage"]
+        plane = Plane.query.filter_by(name=projectile["plane"]).first().id
+        add_projectile(speed=speed, damage=damage, plane_id=plane)
