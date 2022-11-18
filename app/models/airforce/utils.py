4837df2ec5ea3403c5b1373787eab8c746ec526c
AFungo <augustofungo@gmail.com>
2022-11-18 14:39:40 -0300

    add restrictions for load new planes in ini_db method

--- app/models/airforce/utils.py
+++ app/models/airforce/utils.py
@@ -13,9 +13,17 @@ def init_db():
         speed = plane["speed"]
         health = plane["health"]
         cant_projectile = plane["cant_projectile"]
-        add_plane(name, size, speed, health, cant_projectile)
+        # try:
+        print("id", Plane.query.filter_by(name=name).first())
+        if Plane.query.filter_by(name=name).first() == None:
+            add_plane(name, size, speed, health, cant_projectile)
+            # except:
+            None
     for projectile in data["projectile"]:
         speed = projectile["speed"]
         damage = projectile["damage"]
         plane = Plane.query.filter_by(name=projectile["plane"]).first().id
-        add_projectile(speed=speed, damage=damage, plane_id=plane)
+        try:
+            add_projectile(speed=speed, damage=damage, plane_id=plane)
+        except:
+            None
