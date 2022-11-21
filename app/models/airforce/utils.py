740cef2c7fbb16224b360c5ab04d7d00759b4c93
AFungo <augustofungo@gmail.com>
2022-11-21 12:20:02 -0300

    create new method for add users to db from json

--- app/models/airforce/utils.py
+++ app/models/airforce/utils.py
@@ -1,12 +1,15 @@
 import json
 
+from app import db
 from app.daos.airforce.plane_dao import add_plane, add_projectile
+from app.daos.user_dao import add_user, get_user_by_username
 from app.models.airforce.plane import Plane
 
 
-def init_db():
+def init_db_planes():
     f = open("./app/models/airforce/planes.json")
     data = json.load(f)
+    db.create_all()
     for plane in data["planes"]:
         name = plane["name"]
         size = plane["size"]
@@ -27,3 +30,14 @@ def init_db():
             add_projectile(speed=speed, damage=damage, plane_id=plane)
         except:
             None
+
+
+def init_db_users():
+    f = open("./app/models/airforce/planes.json")
+    data = json.load(f)
+    for user in data["users"]:
+        username = user["username"]
+        password = user["password"]
+        email = user["email"]
+        if get_user_by_username(username) == None:
+            add_user(username=username, password=password, email=email)
