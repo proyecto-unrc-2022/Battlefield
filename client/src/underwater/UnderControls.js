9b8ec0455127f78fa4e6ae649e2f5a4c49200fc0
PabloRoss7 <pablorossi2001@gmail.com>
2022-11-22 18:28:27 -0300

    start direction component
    Co-authored-by: Joel D'Autilio <Jota98daut@users.noreply.github.com>
    Co-authored-by: Jonathan Rojo <JoniRojo@users.noreply.github.com>

--- client/src/underwater/UnderControls.js
+++ client/src/underwater/UnderControls.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import AuthService from "../services/auth.service"
+export default function UnderControls({setPosition}) {
+    const params = useParams();
+    const sessionId = params["id"];
+    const user = AuthService.getCurrentUser().sub;
 
-export default function UnderControls() {
+    function DirectionControl() {
+        return (
+            <div className="u-direction-container">
+            </div>
+        );
+    }
+
     return (
-    <div className="u-controls-container">
-        <div className="u-left-controls">
-            <span>Acá van los controles de movimiento</span>
+        <div className="u-controls-container">
+            <div className="u-left-controls">
+                <DirectionControl />
+            </div>
+            <div className="u-right-controls">
+                <span>Acá van los controles de acciones</span>
+            </div>
         </div>
-        <div className="u-right-controls">
-            <span>Acá van los controles de acciones</span>
-        </div>
-    </div>
-    )
+    );
 }
