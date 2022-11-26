b9907c75d9394503b640c6d5e2fd6ac27951c81f
gaxel11 <axel_maicco@hotmail.com>
2022-11-26 07:01:09 -0300

    code restructuring

--- client/src/infantry/components/figureInfantryData.component.js
+++ client/src/infantry/components/figureInfantryData.component.js
@@ -1,52 +1,43 @@
 import React, { Component } from "react";
-import InfantryService from "../services/infantry.service"
+import AuthService from "../../services/auth.service";
+
+/**
+ * Muestra informacion del jugador, como la id, la vida y coordenadas de donde se encuentra
+ */
 export default class FigureInfantryData extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            game_id: props.game_id,
-            user_id: props.user_id,
-            pos_x: null,
-            pos_y: null,
-            hp: null,
-            tamaño: null,
-            velocidad: null,
-            direccion: null,
-            avail_actions: null
+            figure: props.figure
         }
-        
-    }
-    updateData(){
-        let data = InfantryService.getFigure(this.state.user_id, this.state.game_id)
-        data.then((datos) => this.setState({
-            pos_x: datos["data"].pos_x,
-            pos_y: datos["data"].pos_y,
-            hp: datos["data"].hp,
-            tamaño: datos["data"].tamaño,
-            velocidad: datos["data"].velocidad,
-            direccion: datos["data"].direccion,
-            avail_actions: datos["data"].avail_actions
-        }))
-
     }
     render(){
-        this.updateData()
+        let user
+        if(this.state.figure.id_user === AuthService.getCurrentUser().sub){
+            user = "You"
+        }else{
+            user = "Opponent"
+        }
         return <div>
-            <h3>Player {this.state.user_id}</h3>
-            <ul>
-                <li>
-                    HP: {this.state.hp}
-                </li>
-                <li>
-                    x : {this.state.pos_x}
-                </li>
-                <li>
-                    y : {this.state.pos_y}
-                </li>
-                <li>
-                    acciones disponibles : {this.state.avail_actions}
-                </li>
-            </ul>
+        <div class="text-center h4 pb-2 mb-4 text-black border-bottom border-strong-black">
+            <h3>{user}</h3>
         </div>
+        <table class="table table-dark table-striped">
+        <tbody>
+            <tr>
+                <th scope="row">HP:</th>
+                <td>{this.state.figure["hp"]}</td>
+            </tr>
+            <tr>
+                <th scope="row">x:</th>
+                <td>{this.state.figure["pos_x"]}</td>
+            </tr>
+            <tr>
+                <th scope="row">y:</th>
+                <td colspan="2">{this.state.figure["pos_y"]}</td>
+            </tr>
+        </tbody>
+    </table>
+    </div >
     }
 }
