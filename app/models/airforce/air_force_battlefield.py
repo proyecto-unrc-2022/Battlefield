9d1a17f14095517b17657a4fb478e4f0e0bb3907
AFungo <augustofungo@gmail.com>
2022-11-27 22:27:49 -0300

    refactor battlefield, redo move objects in the map logic and remove unused methods

--- app/models/airforce/air_force_battlefield.py
+++ app/models/airforce/air_force_battlefield.py
@@ -1,45 +1,6 @@
+from app.models.airforce.air_force_flying_object import FlyingObject
+from app.models.airforce.airforce_filters import get_player_plane
 from app.models.airforce.plane import Projectile
-
-
-class FlyingObject:
-    player = None
-    flying_obj = None
-    x: int = -1
-    y: int = -1
-    course = -1  # course 1 north, 2 east, 3 south, 4 west
-
-    def __init__(self, player, flying_obj, x, y, course):
-        self.player = player
-        self.flying_obj = flying_obj
-        self.x = x
-        self.y = y
-        self.course = course
-
-    def check_course(self, course):
-        #        raise Exception(self.course)
-        return abs(self.course - course) == 2
-
-    def update_position(self, course):
-        if self.check_course(course):
-            raise ValueError("New course cant be 180 degrees deference")
-        if course == 1:
-            self.y = self.y + self.flying_obj.speed
-        elif course == 2:
-            self.x = self.x + self.flying_obj.speed
-        elif course == 3:
-            self.y = self.y - self.flying_obj.speed
-        elif course == 4:
-            self.x = self.x - self.flying_obj.speed
-
-    def to_dict(self):
-        return {
-            "player": self.player,
-            "flying_obj": self.flying_obj.id,
-            "x": self.x,
-            "y": self.y,
-            "course": self.course,
-            "flying_obj_class": self.flying_obj.__class__.__name__,
-        }
 
 
 class Battlefield:
@@ -52,6 +13,13 @@ class Battlefield:
         self.max_x = 20
         self.max_y = 10
 
+    def add_new_flying_object(self, player, obj, x, y, course):
+        if not self.position_inside_map(x, y):
+            raise Exception("Invalid position")
+        fly_obj = FlyingObject(player, obj, x, y, course)
+        self.flying_objects.append(fly_obj)
+        return fly_obj
+
     def position_inside_map(self, x, y):
         if x > self.max_x:
             return False
@@ -63,77 +31,100 @@ class Battlefield:
             return False
         return True
 
-    @classmethod
-    def position_inside_player_field(self, x, y, course, player, air_force_game):
-        if air_force_game.player_a == player:
-            if x > self.max_x / 2:
-                return False
-            if (x == (self.max_x / 2)) and course != 2:
-                return False
-        if air_force_game.player_b == player:
-            if x <= self.max_x / 2:
-                return False
-            if (x == (self.max_x / 2) + 1) and course != 4:
-                return False
+    def move(self, fly_obj, course):
+        self.check_colision(fly_obj, course)
+        try:
+            self.flying_objects.index(fly_obj)
+            fly_obj.update_position(course, self.max_x, self.max_y)
+        except:
+            None
 
-        return True
+    def check_colision(self, fly_obj, course):
+        if course == 1 or course == 3:
+            self.colision_y(fly_obj, course)
+        elif course == 2 or course == 4:
+            self.colision_x(fly_obj, course)
 
-    def add_new_plane(self, player, obj, x, y, course, air_force_game):
-        if not self.position_inside_map(x, y):
-            raise Exception("Invalid position")
-
-        if not self.position_inside_player_field(x, y, course, player, air_force_game):
-            raise Exception("Plane position cant be inside enemy field")
-
-        if self.player_have_plane(player):
-            raise Exception("This player already have a plane")
-
-        print("\n\n\n\n\n Plane: ", obj, " \n\n\n\n\n" )
-        fly_obj = FlyingObject(player, obj, x, y, course)
-        self.flying_objects.append(fly_obj)
-        return fly_obj
-
-    def player_have_plane(self, player):
-        return self.get_player_plane(player) != []
-
-    def get_player_plane(self, player):
-        return list(
-            filter(
-                lambda x: int(x.player) == int(player)
-                and x.flying_obj.__class__.__name__ == "Plane",
-                self.flying_objects,
+    def colision_x(self, fly_obj, course):
+        position = fly_obj.x
+        if course == 2:
+            speed = fly_obj.flying_obj.speed
+            colision_obj = list(
+                filter(
+                    lambda object: object.x > position
+                    and object.x <= position + speed
+                    and object.y == fly_obj.y,
+                    self.flying_objects,
+                )
             )
-        )
+        else:
+            speed = -fly_obj.flying_obj.speed
+            colision_obj = list(
+                filter(
+                    lambda object: object.x < position
+                    and object.x >= position + speed
+                    and object.y == fly_obj.y,
+                    self.flying_objects,
+                )
+            )
+        if colision_obj != []:
+            obj = min(colision_obj, key=lambda obj: obj.x + speed)
+            self.colision(fly_obj, obj)
 
-    def fligth(self, player, course):
-        obj = self.get_player_plane(player)[0]
-        self.update_plane_position(course, obj)
-        return obj
-
-    def add_new_projectile(self, player):
-        plane = self.get_player_plane(int(player))[0]
-        course = plane.course
-        x = plane.x
-        y = plane.y
-
-        plane_model = plane.flying_obj
-        obj = Projectile.query.filter_by(plane_id=plane_model.id).first()
-        print("\n\n\n plane: ", plane_model.name, "\n\n\n projectile: ", obj)
-        # obj = plane_model.projectile[0]
-
-        print("\n\n\nplayer: ", player, "plane: ", plane)
-
-        fly_obj = FlyingObject(player, obj, x, y, course)
+    def colision_y(self, fly_obj, course):
+        position = fly_obj.y
         if course == 1:
-            fly_obj.y = fly_obj.y - 1
-        elif course == 2:
-            fly_obj.x = fly_obj.x + 1
-        elif course == 3:
-            fly_obj.y = fly_obj.y + 1
-        elif course == 4:
-            fly_obj.x = fly_obj.x - 1
-        self.flying_objects.append(fly_obj)
-        return fly_obj
+            speed = fly_obj.flying_obj.speed
+            colision_obj = list(
+                filter(
+                    lambda p: p.y > position
+                    and p.y <= position + speed
+                    and p.x == fly_obj.x,
+                    self.flying_objects,
+                )
+            )
+        else:
+            speed = -fly_obj.flying_obj.speed
+            colision_obj = list(
+                filter(
+                    lambda p: p.y < position
+                    and p.y >= position + speed
+                    and p.x == fly_obj.x,
+                    self.flying_objects,
+                )
+            )
+        if colision_obj != []:
+            obj = min(colision_obj, key=lambda obj: obj.y + speed)
+            self.colision(fly_obj, obj)
+
+    def colision(self, crashing, crashed):
+        if crashed.flying_obj.__class__.__name__ == "Projectile":
+            self.projectile_collision(crashed, crashing)
+        elif crashing.flying_obj.__class__.__name__ == "Projectile":
+            self.projectile_collision(crashing, crashed)
+        else:
+            self.plane_collision(crashing, crashed)
+
+    def plane_collision(self, crashing, crashed):
+        crashed_health = crashed.flying_obj.health
+        crashed.flying_obj.health -= crashing.flying_obj.health
+        crashing.flying_obj.health -= crashed_health
+        self.flying_objects.remove(crashed) if (
+            crashed.flying_obj.health <= 0
+        ) else True
+        self.flying_objects.remove(crashing) if (
+            crashing.flying_obj.health <= 0
+        ) else True
+
+    def projectile_collision(self, projectile, fly_obj):
+        if fly_obj.flying_obj.__class__.__name__ == "Plane":
+            fly_obj.flying_obj.health -= projectile.flying_obj.damage
+            self.flying_objects.remove(fly_obj) if (
+                fly_obj.flying_obj.health <= 0
+            ) else True
+        else:
+            self.flying_objects.remove(fly_obj)
+        self.flying_objects.remove(projectile)
 
     def move_projectile(self, player):
         obj = list(
@@ -143,154 +134,15 @@ class Battlefield:
                 self.flying_objects,
             )
         )
-        for n in range(len(obj)):
-            course = obj[n].course
-            if course == 2 or course == 4:
-                tupl = self.collision_x_projectile(obj[n], course)
-                if tupl != ():
-                    print("paseee por aca")
-                    self.projectile_collision(tupl[0].tupl[1])
-                else:
-                    obj[n].update_position(course)
-                    if obj[n].x >= 20 or obj[n].x <= 0:
-                        self.flying_objects.remove(obj[n])
-            if course == 1 or course == 3:
-                tupl = self.collision_y_projectile(obj[n], course)
-                if tupl != ():
-                    self.projectile_collision(tupl[0], tupl[1])
-                else:
-                    obj[n].update_position(course)
-                    if obj[n].y >= 10 or obj[n].y <= 0:
-                        self.flying_objects.remove(obj[n])
-
-    def projectile_collision(self, projectile, fly_obj):
-        print("planeeee", fly_obj)
-        if fly_obj.__class__.__name__ == "Plane":
-            fly_obj.health -= projectile.damage
-            self.flying_objects.remove if (fly_obj.health <= 0) else True
-        else:
-            self.flying_objects.remove(fly_obj)
-        self.flying_objects.remove(projectile)
-
-    def collision_x_projectile(self, fly_obj, course):
-        position = fly_obj.x
-        speed = fly_obj.flying_obj.speed if course == 2 else -fly_obj.flying_obj.speed
-        if speed > 0:
-            projectile_coll = list(
-                filter(
-                    lambda p: p.y == fly_obj.y
-                    and p.x > position
-                    and p.x <= position + speed,
-                    self.flying_objects,
-                )
-            )
-            print("size: ", projectile_coll)
-            if projectile_coll != []:
-                min_distance = min(projectile_coll, key=lambda p: p.x)
-                return (fly_obj, min_distance)
-        else:
-            projectile_coll = list(
-                filter(
-                    lambda p: p.y == fly_obj.y
-                    and p.x < position
-                    and p.x >= position + speed,
-                    self.flying_objects,
-                )
-            )
-            if projectile_coll != []:
-                min_distance = min(projectile_coll, key=lambda p: p.x)
-                return (fly_obj, min_distance)
-        return ()
-
-    def collision_y_projectile(self, fly_obj, course):
-        position = fly_obj.y
-        speed = fly_obj.flying_obj.speed if course == 1 else -fly_obj.flying_obj.speed
-        if speed > 0:
-            projectile_coll = list(
-                filter(
-                    lambda p: p.x == fly_obj.x
-                    and p.y > position
-                    and p.y <= position + speed,
-                    self.flying_objects,
-                )
-            )
-            if projectile_coll != []:
-                min_distance = min(projectile_coll, key=lambda p: p.y)
-                return (fly_obj, min_distance)
-        else:
-            projectile_coll = list(
-                filter(
-                    lambda p: p.x == fly_obj.x
-                    and p.y < position
-                    and p.y >= position + speed,
-                    self.flying_objects,
-                )
-            )
-            if projectile_coll != []:
-                min_distance = min(projectile_coll, key=lambda p: p.y)
-                return (fly_obj, min_distance)
-        return ()
+        for n in obj:
+            self.move(n, n.course)
 
     def check_course(self, course, player):
-        obj = self.get_player_plane(int(player))
+        obj = get_player_plane(self, int(player))
         if obj != []:
-            if obj[0].check_course(int(course)):
+            print(course)
+            if obj[0].check_invalid_course(int(course)):
                 raise ValueError("New course cant be 180 degrees deference")
-
-    def update_plane_position(self, course, fly_obj):
-        if fly_obj.check_course(course):
-            raise ValueError("New course cant be 180 degrees deference")
-        fly_obj.course = course
-        if course == 1:
-            self.colision_y(fly_obj, course)
-            new_y = fly_obj.y + fly_obj.flying_obj.speed
-            fly_obj.y = new_y if new_y <= self.max_y else self.max_y
-        elif course == 2:
-            self.colision_x(fly_obj, course)
-            new_x = fly_obj.x + fly_obj.flying_obj.speed
-            fly_obj.x = new_x if new_x <= self.max_x else self.max_x
-        elif course == 3:
-            self.colision_y(fly_obj, course)
-            new_y = fly_obj.y - fly_obj.flying_obj.speed
-            fly_obj.y = new_y if new_y >= 0 else 0
-        elif course == 4:
-            self.colision_x(fly_obj, course)
-            new_x = fly_obj.x - fly_obj.flying_obj.speed
-            fly_obj.x = new_x if new_x >= 0 else 0
-
-    def colision_x(self, fly_obj, course):
-        position = fly_obj.x
-        speed = fly_obj.flying_obj.speed if course == 1 else -fly_obj.flying_obj.speed
-
-        colision_obj = list(
-            filter(
-                lambda x: x.x < position and x.x >= position + speed,
-                self.flying_objects,
-            )
-        )
-        if colision_obj != []:
-            obj = min(colision_obj, key=lambda x: x.x + speed)
-            if obj.flying_obj.__class__.__name__ == "Projectile":
-                self.projectile_collision(obj, fly_obj)
-            else:
-                print(obj.__class__.__name__)
-                obj.flying_obj.health -= fly_obj.flying_obj.health
-                fly_obj.flying_obj.health -= obj.flying_obj.health
-
-    def colision_y(self, plane, course):
-        position = plane.y
-        speed = plane.flying_obj.speed if course == 2 else -plane.flying_obj.speed
-        colision_obj = list(
-            filter(
-                lambda p: p.y < position and p.y >= position + speed,
-                self.flying_objects,
-            )
-        )
-        if colision_obj != []:
-            obj = min(colision_obj, key=lambda x: x.x + speed)
-            # raise Exception('A = ', obj.to_dict(), plane.to_dict())
-            obj.flying_obj.health -= plane.flying_obj.health
-            plane.flying_obj.health -= obj.flying_obj.health
 
     def get_status(self):
         l = {}
