81b76e2a17aae97425336ed08e7177b68fc5c09b
Nacho521 <juanignacioromero123456@gmail.com>
2022-11-28 09:55:55 -0300

    Add visibility

--- client/src/infantry/components/gameBoard.component.js
+++ client/src/infantry/components/gameBoard.component.js
@@ -20,23 +20,23 @@ export default class GameBoard extends Component {
     }
   }
   //Actualiza el estado de las figuras y los projectiles
-  componentDidUpdate(prevProps){
-    if(prevProps.figure !== this.props.figure || prevProps.figureOpponent !== this.props.figureOpponent
-      || prevProps.projectiles !== this.props.projectiles){
-        this.setState({
-          figure: this.props.figure,
-          figureOpponent: this.props.figureOpponent,
-          projectiles: this.props.projectiles
-        })
-      }
+  componentDidUpdate(prevProps) {
+    if (prevProps.figure !== this.props.figure || prevProps.figureOpponent !== this.props.figureOpponent
+      || prevProps.projectiles !== this.props.projectiles) {
+      this.setState({
+        figure: this.props.figure,
+        projectiles: this.props.projectiles
+      })
+    }
   }
+
   /**
-   * Verifica si las coordenadas estan dentro del mapa
-   * @param {Array} coor coordenadas
-   * @returns True si las coordenadas estan dentro del mapa, False en caso contrario
-   */
-  is_it_on_map(coor){
-    if(coor[0] >= 0 && coor[0] < 20 && coor[1] >= 0 && coor[1] < 10){
+  * Verifica si las coordenadas estan dentro del mapa
+  * @param {Array} coor coordenadas
+  * @returns True si las coordenadas estan dentro del mapa, False en caso contrario
+  */
+  is_it_on_map(coor) {
+    if (coor[0] >= 0 && coor[0] < 20 && coor[1] >= 0 && coor[1] < 10) {
       return true
     }
     return false
@@ -47,35 +47,82 @@ export default class GameBoard extends Component {
    * representado por un arreglo y tambien las figuras y projectiles, 0 representando una casilla terrestre
    * @returns Tablero del juego con los datos del estado actual del componente
    */
-drawGameBoard(){
-  let board = (new Array(10)).fill().map(function(){ return new Array(20).fill(0);});
-  this.state.figure.map(coor => {if(this.is_it_on_map(coor)){board[coor[1]][coor[0]] = FIGURE}})
-  this.state.figureOpponent.map(coor => {if(this.is_it_on_map(coor)){board[coor[1]][coor[0]] = FIGURE}})
-  if(this.state.projectiles !== undefined){
-    console.log(this.state.projectiles)
-    this.state.projectiles.map(projectile => board[projectile["pos_y"]][projectile["pos_x"]] = PROJECTILE)
+  drawGameBoard() {
+    let board = (new Array(10)).fill().map(function () { return new Array(20).fill(0); });
+    this.state.figure.map(coor => { if (this.is_it_on_map(coor)) { board[coor[1]][coor[0]] = FIGURE } })
+    this.state.figureOpponent.map(coor => { if (this.is_it_on_map(coor)) { board[coor[1]][coor[0]] = FIGURE } })
+    if (this.state.projectiles !== undefined) {
+      this.state.projectiles.map(projectile => board[projectile["pos_y"]][projectile["pos_x"]] = PROJECTILE)
+    }
+
+    return board
   }
-  return board
-}
+
   render() {
-    const board = this.drawGameBoard()
+
+    let board = this.drawGameBoard()
+
     const casillas = [];
+
     for (let i = 0; i < board.length; i++) {
       const mapItem = [];
       for (let j = 0; j < board[i].length; j++) {
-
-        if(board[i][j] === 1){
-          mapItem.push(<li class="square p-3 bg-dark"></li>);
+        if (board[i][j] === 1) {
+          mapItem.push(<li class="square p-3 bg-primary"></li>);
         }
-        else if(board[i][j] === PROJECTILE){
+        else if ((board[i][j] === PROJECTILE)) {
           mapItem.push(<li class="square p-3 bg-danger"></li>);
         }
-        else{
+
+        else if (board[i][j + 2] === 1) {
+          mapItem.push(<li class="square p-3 bg-white"></li>);
+        }
+        else if (board[i][j + 2] === 1) {
+          mapItem.push(<li class="square p-3 bg-white"></li>);
+        }
+        else if (board[i][j + 1] === 1) {
+          mapItem.push(<li class="square p-3 bg-white"></li>);
+        }
+        else if (board[i][j - 2] === 1) {
+          mapItem.push(<li class="square p-3 bg-white"></li>);
+        }
+        else if (board[i][j - 1] === 1) {
+          mapItem.push(<li class="square p-3 bg-white"></li>);
+        }
+        else if (i > 0 && board[i - 1][j] === 1) {
+          mapItem.push(<li class="square p-3 bg-white"></li>);
+        }
+        else if (i > 1 && board[i - 2][j] === 1) {
+          mapItem.push(<li class="square p-3 bg-white"></li>);
+        }
+        else if (i < 7 && board[i + 2][j] === 1) {
+          mapItem.push(<li class="square p-3 bg-white"></li>);
+        }
+        else if (i < 8 && board[i + 1][j] === 1) {
+          mapItem.push(<li class="square p-3 bg-white"></li>);
+        }
+        else if (i < 8 && board[i + 1][j - 1] === 1) {
+          mapItem.push(<li class="square p-3 bg-white"></li>);
+        }
+        else if (i < 8 && board[i + 1][j + 1] === 1) {
+          mapItem.push(<li class="square p-3 bg-white"></li>);
+        }
+        else if (i > 0 && board[i - 1][j + 1] === 1) {
+          mapItem.push(<li class="square p-3 bg-white"></li>);
+        }
+        else if (i > 0 && board[i - 1][j - 1] === 1) {
+          mapItem.push(<li class="square p-3 bg-white"></li>);
+        }
+        else {
           mapItem.push(<li class="square p-3"></li>);
         }
       }
       casillas.push(<ul class="list-unstyled">{mapItem}</ul>);
     }
+
     return casillas;
+
+
+
   }
 }
