2d23d015277e3027d914adf9f3ee6754835d3a2e
Nacho521 <juanignacioromero123456@gmail.com>
2022-11-28 22:19:25 -0300

    Add for selections velocity for artillery

--- client/src/infantry/components/gameInfantry.component.js
+++ client/src/infantry/components/gameInfantry.component.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import GameBoard from "./gameBoard.component";
 import FigureInfantryData from "./figureInfantryData.component";
 import InfantryService from "../services/infantry.service"
-import {Link, Navigate} from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 import AuthService from "../../services/auth.service";
 import gameOver from "../images/gameOver.png"
 import gameService from "../services/game.service";
@@ -161,6 +161,7 @@ export default class GameInfantry extends Component {
    * @param {int} velocity alcance de la accion(solo sirve para la accion mover, o para el proyectil de la artilleria)
    */
   async action(direction, action, velocity) {
+    console.log("entre",velocity)
     if (action === "move") {
       let response = await InfantryService.move(this.state.game_id, AuthService.getCurrentUser().sub,
         direction, velocity)
@@ -172,7 +173,7 @@ export default class GameInfantry extends Component {
     }
     if (action === "shoot") {
       let response = await InfantryService.shoot(this.state.game_id, AuthService.getCurrentUser().sub,
-        direction)
+        direction, velocity)
       if (response === "Accion invalida") {
         return alert("Movimiento invalido")
       }
@@ -257,6 +258,14 @@ export default class GameInfantry extends Component {
     }
     return options
   }
+
+  optionsVelocity(){
+    const options = []
+    for (let i = 3; i <= 20; i++) {
+      options.push(<option value={i}>{i}</option>)
+    }
+    return options
+  }
   /**
    * Muestra un mensaje dependiendo de la situacion de la ronda
    * @returns Retorna dos mensajes dependiendo de si acabo la ronda o no
@@ -285,8 +294,6 @@ export default class GameInfantry extends Component {
 
   GameOver() {
     if (this.state.figure !== null && this.state.figureOpponent !== null) {
-      console.log(this.state.figure["data"].hp)
-      console.log(this.state.figureOpponent["data"].hp)
       if (this.state.figure["data"].hp <= 0 || this.state.figureOpponent["data"].hp <= 0) {
         console.log("Entre")
         return true
@@ -320,112 +327,128 @@ export default class GameInfantry extends Component {
     } else if (this.GameOver()) {
       return (<div class="text-center bg-War">
         <img src={gameOver} />
-        <p>{this.state.figure["data"].hp <= 0 ? this.state.figure["data"].id_user: 
-                                                this.state.figureOpponent["data"].id_user}</p>
+        <p>{this.state.figure["data"].hp <= 0 ? this.state.figure["data"].id_user :
+          this.state.figureOpponent["data"].id_user}</p>
       </div>)
 
     }
     else {
       return (
         <div>
-        <div class="container-fluid bg-War">
-          <div class="row align-items-start">
-            <div class="col-4 mx-3 mt-5"><FigureInfantryData figure={this.state.figure["data"]} /></div>
-            <div> <GameBoard figure={this.state.figure["body"]} figureOpponent={this.state.figureOpponent["body"]} projectiles={this.state.projectiles}/></div>
+          <div class="container-fluid bg-War">
+            <div class="row align-items-start">
+              <div class="col-4 mx-3 mt-5"><FigureInfantryData figure={this.state.figure["data"]} /></div>
+              <div> <GameBoard figure={this.state.figure["body"]} figureOpponent={this.state.figureOpponent["body"]} projectiles={this.state.projectiles} /></div>
 
-          </div>
-          <p class="text-center">{this.getMessageTurn()}</p>
-          <div class="row">
-        
-            <div class="container col">
-              {/* Formulario para que el usuario elija que accion desea realizar */}
-              <form onSubmit={ev => {
-                ev.preventDefault();
-                this.setState({
-                  next_turn: true
-                })
-                this.action(ev.target.direction.value, ev.target.action.value, ev.target.velocity.value)
-              }} >
-                {/* botones para seleccionar la direccion */}
-                <br></br>
-                <div class="form-group ">
-                  <div class="row justify-content-center">
-                    <div>
-                      <input type="radio" class="btn-check col" name="direction" id="north west" autocomplete="off" checked value={NORTH_WEST} />
-                      <label class="btn btn-secondary col" for="north west">North west</label>
-
-                    </div>
-                    <div>
-                      <input type="radio" class="btn-check col" name="direction" id="north" autocomplete="off" checked value={NORTH} />
-                      <label class="btn btn-secondary col" for="north">North</label>
-                    </div>
-                    <div>
-                      <input type="radio" class="btn-check col" name="direction" id="north east" autocomplete="off" checked value={NORTH_EAST} />
-                      <label class="btn btn-secondary col" for="north east">North east</label>
-                    </div>
-                  </div>
-
-                  <div class="row justify-content-center">
-                    <div class="mx-5">
-                      <input type="radio" class="btn-check mx-3" name="direction" id="west" autocomplete="off" checked value={WEST} />
-                      <label class="btn btn-secondary" for="west">West</label>
-                    </div>                      
-                    <div className="col-1">
-
-                    </div>
-                    <div class="mx-5">
-                        
-                      <label class="btn btn-secondary" for="east">East</label>
-                      <input type="radio" class="btn-check mx-3" name="direction" id="east" autocomplete="off" checked value={EAST} />
-                    </div>
-                      
-                    
-                  </div>
-
-                  <div class="row justify-content-center">
-                    <div>
-                      <label class="btn btn-secondary col" for="south west" >South west</label>
-                      <input type="radio" class="btn-check col" name="direction" id="south west" autocomplete="off" checked value={SOUTH_WEST} />
-                    </div>
-                    <div>
-                      <label class="btn btn-secondary col " for="south">South</label>
-                      <input type="radio" class="btn-check col" name="direction" id="south" autocomplete="off" checked value={SOUTH} />
-                    </div>
-                    <div>
-                      <label class="btn btn-secondary col" for="south east">South east</label>
-                      <input type="radio" class="btn-check col" name="direction" id="south east" autocomplete="off" checked value={SOUTH_EAST} />
-                    </div>
-                  </div>
-                </div>
-                {/* botones para seleccionar la accion */}
-                <br></br>
-                <div className="row justify-content-center">
-                  <div class="form-check">
-                    <input class="form-check-input" type="radio" name="action" id="move" value={"move"} />
-                    <label class="form-check-label text-white" for="move">
-                      Move
-                      <select class="form-select" id="sel1" name="velocity">
-                        {this.optionsRender()}
-                      </select>
-                    </label>
-                    <input class="form-check-input" type="radio" name="action" id="shoot" value={"shoot"} />
-                    <label class="form-check-label text-white" for="shoot">
-                      Shoot
-                    </label>
-                  </div>
-                </div>
-                
-                <br></br><br></br><br></br>
-                <div class="text-center position-absolute top-50 start-50 translate-middle col">
-                  <button type="submit" class="btn btn-outline-light" disabled={this.state.finished_round || this.state.game.turn !== AuthService.getCurrentUser().sub}>Next turn</button>
-                </div>
-              </form>
             </div>
-            
+            <p class="text-center">{this.getMessageTurn()}</p>
+            <div class="row">
+
+              <div class="container col">
+                {/* Formulario para que el usuario elija que accion desea realizar */}
+                <form onSubmit={ev => {
+                  ev.preventDefault();
+                  this.setState({
+                    next_turn: true
+                  })
+                  this.action(ev.target.direction.value, ev.target.action.value, ev.target.velocity.value)
+                }} >
+                  {/* botones para seleccionar la direccion */}
+                  <br></br>
+                  <div class="form-group ">
+                    <div class="row justify-content-center">
+                      <div>
+                        <input type="radio" class="btn-check col" name="direction" id="north west" autocomplete="off" checked value={NORTH_WEST} />
+                        <label class="btn btn-secondary col" for="north west">North west</label>
+
+                      </div>
+                      <div>
+                        <input type="radio" class="btn-check col" name="direction" id="north" autocomplete="off" checked value={NORTH} />
+                        <label class="btn btn-secondary col" for="north">North</label>
+                      </div>
+                      <div>
+                        <input type="radio" class="btn-check col" name="direction" id="north east" autocomplete="off" checked value={NORTH_EAST} />
+                        <label class="btn btn-secondary col" for="north east">North east</label>
+                      </div>
+                    </div>
+
+                    <div class="row justify-content-center">
+                      <div class="mx-5">
+                        <input type="radio" class="btn-check mx-3" name="direction" id="west" autocomplete="off" checked value={WEST} />
+                        <label class="btn btn-secondary" for="west">West</label>
+                      </div>
+                      <div className="col-1">
+
+                      </div>
+                      <div class="mx-5">
+
+                        <label class="btn btn-secondary" for="east">East</label>
+                        <input type="radio" class="btn-check mx-3" name="direction" id="east" autocomplete="off" checked value={EAST} />
+                      </div>
+
+
+                    </div>
+
+                    <div class="row justify-content-center">
+                      <div>
+                        <label class="btn btn-secondary col" for="south west" >South west</label>
+                        <input type="radio" class="btn-check col" name="direction" id="south west" autocomplete="off" checked value={SOUTH_WEST} />
+                      </div>
+                      <div>
+                        <label class="btn btn-secondary col " for="south">South</label>
+                        <input type="radio" class="btn-check col" name="direction" id="south" autocomplete="off" checked value={SOUTH} />
+                      </div>
+                      <div>
+                        <label class="btn btn-secondary col" for="south east">South east</label>
+                        <input type="radio" class="btn-check col" name="direction" id="south east" autocomplete="off" checked value={SOUTH_EAST} />
+                      </div>
+                    </div>
+                  </div>
+                  {/* botones para seleccionar la accion */}
+                  <br></br>
+                  <div className="row justify-content-center">
+                    <div class="form-check">
+                      <input class="form-check-input" type="radio" name="action" id="move" value={"move"} />
+                      <label class="form-check-label text-white" for="move">
+                        Move
+                        <select class="form-select" id="sel1" name="velocity">
+                          {this.optionsRender()}
+                        </select>
+                      </label>
+                      {this.state.figure["data"].figure_type === 4 ?
+                        <div>
+                        
+                          <input class="form-check-input" type="radio" name="action" id="shoot" value={"shoot"} />
+                          <label class="form-check-label text-white" for="shoot">
+                            Shoot
+                          <select class="form-select" id="sel1" name="velocity">
+                            {this.optionsVelocity()}
+                          </select>
+                          </label>
+                        </div>
+                        :
+                        <div>
+                          <input class="form-check-input" type="radio" name="action" id="shoot" value={"shoot"} />
+                          <label class="form-check-label text-white" for="shoot">
+                            Shoot
+                          </label>
+                        </div>
+                      }
+
+                    </div>
+                  </div>
+
+                  <br></br><br></br><br></br>
+                  <div class="text-center position-absolute top-50 start-50 translate-middle col">
+                    <button type="submit" class="btn btn-outline-light" disabled={this.state.finished_round || this.state.game.turn !== AuthService.getCurrentUser().sub}>Next turn</button>
+                  </div>
+                </form>
+              </div>
+
+            </div>
+
           </div>
-          
-        </div>    
-      </div>
+        </div>
       )
     }
   }
