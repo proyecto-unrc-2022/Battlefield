20a71d0a891d372f4320c1ec1f83dd9eb271a454
Nacho521 <juanignacioromero123456@gmail.com>
2022-11-29 04:03:27 -0300

    Add steps of scenarios

--- features/steps/infantry_step.py
+++ features/steps/infantry_step.py
@@ -47,141 +47,278 @@ def step_impl(context):
 
 @then('he is the second player')
 def step_impl(context):
-    game = Game_Infantry.query.filter_by(id = 1).first()
-    assert game.id_user2 == 2
-    
-# @given('a user Franco')
-# def step_impl(context):
-#     user1 = User(username= "Franco", email="Franco@gmail.com", password="123")
-#     add_user(user1.username, user1.password, user1.email)
-#     user2 = User(username= "Tomas", email="Tomas@gmail.com", password="123")
-#     add_user(user2.username, user2.password, user2.email)
-#     user = User.query.filter_by(id = 1).first()
-#     assert not(user == None)
+    headers = {"Content-Type": "application/json"}
+    context.page = context.client.post(url_for("infantry.ready_to_play"), data = json.dumps(
+    {"game_id" : 1, "user_id" : 2}), headers=headers)
+    assert context.page.status_code == 200
 
-# @when('you choose your soldier')
-# def step_impl(context):
-#     context.page = context.client.post(url_for("infantry.choose_figure", game_id = 1, user_id = 1, type= 1),
-#         data = json.dumps({"pos_x" : 1, "pos_y" : 1}))
-#     assert context.page.status_code == 200
-    
-# @then('the soldier is created')
-# def step_impl(context):
-#     ob = Figure_infantry.query.filter_by(id_user = 1, id_game = 1).first()
-#     context.ob = ob
-#     is_created = context.ob.id == 1
-#     is_soldier = context.ob.figure_type == 1
-#     assert is_created and is_soldier
+@given('user any')
+def step_impl(context):
+    headers = {"Content-Type": "application/json"}
+    context.page = context.client.post(url_for("users.create_user"), data = json.dumps(
+        {"username" : "Eduardo", "email" : "Eduardo@gmail.com", "password" : "123"}), headers=headers)
+    assert context.page.status_code == 200
 
-# @given('a user Tomas')
-# def step_impl(context):
-#     user = user = User.query.filter_by(username = "Tomas").first()
-#     assert not(user == None)
+@when('you create a game')
+def step_impl(context):
+    context.page = context.client.post(url_for("infantry.start_game",user_id= 1))
+    assert context.page.status_code == 200
 
-# @when('you choose your Humvee')
-# def step_impl(context):
-#     context.page = context.client.post(url_for("infantry.choose_figure", game_id = 1, user_id = 2, figure_id= 2))
-#     assert context.page.status_code == 200
-    
-# @then('the humvee is created')
-# def step_impl(context):
-#     ob = Figure_infantry.query.filter_by(id_user = 2, id_game = 1).first()
-#     context.ob = ob
-#     is_created = context.ob.id == 2
-#     is_humvee = context.ob.figure_type == 2
-#     assert is_created and is_humvee
+@then('he can not join a game that he created himself')
+def step_impl(context):
+    context.page = context.client.post(url_for("infantry.join_game",game_id= 1, user_id= 1))
+    assert context.page.status_code == 404
 
-# @given('a user Lucas')
-# def step_given(context) :
-#     user = User(username= "Lucas", email="Lucas@gmail.com", password="1234")
-#     add_user(user.username, user.password, user.email)
-#     assert True   
-    
-# @when('you choose your tank')
-# def step_when(context) :
-#     context.page = context.client.post(url_for("infantry.choose_entity",entity_id= 3))
-#     assert context.page.status_code == 200
-    
-# @then('the tank is created')
-# def step_then(context) :
-#     ob = Figure_infantry.query.order_by(Figure_infantry.id.desc()).first()
-#     context.ob = ob
-#     is_created = context.ob.id == 3
-#     is_tank = Figure_infantry.query.filter_by(id=3).first().figure_type == 3
-#     assert is_created and is_tank
-  
-# @given('a user Ricardo')
-# def step_given(context) :
-#     user = User(username= "Ricardo", email="Ricardo@gmail.com", password="1324")
-#     add_user(user.username, user.password, user.email)
-#     assert True   
+##################################################### FIGURE ########################################################################
+@given('a user with the corresponding coordinates')
+def step_impl(context):
+    headers = {"Content-Type": "application/json"}
+    context.page = context.client.post(url_for("users.create_user"), data = json.dumps(
+        {"username" : "Franco", "email" : "Franco@gmail.com", "password" : "123"}), headers=headers)
+    assert context.page.status_code == 200
 
-# @when('you choose your artillery')
-# def step_when(context) :
-#     context.page = context.client.post(url_for("infantry.choose_entity",entity_id=4))
-#     assert context.page.status_code == 200 
+@when('you choose the figure and it is created')
+def step_impl(context):
+    #The game must have a user 2
+    game = Game_Infantry(id_user1= 1, id_user2= 2)
+    db.session.add(game)
+    db.session.commit() 
+    headers = {"Content-Type": "application/json"}
+    context.page = context.client.post(url_for("infantry.choose_figure", game_id = 1, user_id = 1, type= "1"), data = json.dumps(
+        {"pos_x" : 1, "pos_y" : 1}), headers=headers)
+    assert context.page.status_code == 200
 
-# @then('the artillery is created')
-# def step_then(context) :
-#     ob = Figure_infantry.query.order_by(Figure_infantry.id.desc()).first()
-#     context.ob = ob
-#     is_created = context.ob.id == 4
-#     is_artillery = Figure_infantry.query.filter_by(id=4).first().figure_type == 4
-#     assert is_created and is_artillery
+@then('wait the opponent')
+def step_impl(context):
+    #When creating the figure then you should only wait for the opponent
+    assert True
 
+@given('a user with the wrong coordinates')
+def step_impl(context):
+    headers = {"Content-Type": "application/json"}
+    context.page = context.client.post(url_for("users.create_user"), data = json.dumps(
+        {"username" : "Damian", "email" : "Damian@gmail.com", "password" : "123"}), headers=headers)
+    assert context.page.status_code == 200
 
-# @when('you choose to move your unit east')
-# def step_when(context) :
-#     context.page = context.client.post(url_for("infantry.mov_action", game_id = 1, user_id = 1, velocity = 2, direction = EAST))
-#     assert context.page.status_code == 200    
+@when('you choose the figure')
+def step_impl(context):
+    #The game must have a user 2
+    game = Game_Infantry(id_user1= 1, id_user2= 2)
+    db.session.add(game)
+    db.session.commit() 
+    headers = {"Content-Type": "application/json"}
+    context.page = context.client.post(url_for("infantry.choose_figure", game_id = 1, user_id = 1, type= "1"), data = json.dumps(
+        {"pos_x" : 12, "pos_y" : 1}), headers=headers)
+    assert context.page.status_code == 404
 
-# @then('the unit moves east')
-# def step_then(context) :
-#     figure = Figure_infantry.query.filter_by(id_game = 1, id_user = 1).first()
-#     assert figure.pos_x == 2 and figure.pos_y == 0
-    
-# @when('you choose to move your unit west')
-# def step_when(context) :
-#     figure = Figure_infantry.query.filter_by(id_game = 1, id_user = 2).first()
-#     context.page = context.client.post(url_for("infantry.mov_action", game_id = 1, user_id = 2, velocity = 2, direction = NORTH))
-#     assert context.page.status_code == 200
-    
-# @then('the unit moves west')
-# def step_then(context) :
-#     figure = Figure_infantry.query.filter_by(id_game = 1, id_user = 2).first()
-#     assert figure.pos_y == 2 and figure.pos_x == 0
+@then('it couldn not have been created')
+def step_impl(context):
+    #Since the figure is not created then the scenario is true
+    assert True
 
-# @when('choose an invalid move')
-# def step_when(context) :
-#     figure = Figure_infantry.query.filter_by(id_game = 1, id_user = 2).first()
-#     context.page = context.client.post(url_for("infantry.mov_action", game_id = 1, user_id = 2, velocity = 2, direction = SOUTH_EAST))
-#     assert context.page.status_code == 404
+@given('a user x')
+def step_impl(context):
+    headers = {"Content-Type": "application/json"}
+    context.page = context.client.post(url_for("users.create_user"), data = json.dumps(
+        {"username" : "Juan", "email" : "Juan@gmail.com", "password" : "123"}), headers=headers)
+    assert context.page.status_code == 200
 
-# @then('the unit does not move')
-# def step_then(context) :
-#     figure = Figure_infantry.query.filter_by(id_game = 1, id_user = 2).first()
-#     assert figure.pos_y == 2 and figure.pos_x == 0
+@when('you select the type 1 figure')
+def step_impl(context):
+    #The game must have a user 2
+    game = Game_Infantry(id_user1= 1, id_user2= 2)
+    db.session.add(game)
+    db.session.commit() 
+    headers = {"Content-Type": "application/json"}
+    context.page = context.client.post(url_for("infantry.choose_figure", game_id = 1, user_id = 1, type= "1"), data = json.dumps(
+        {"pos_x" : 1, "pos_y" : 1}), headers=headers)
+    assert context.page.status_code == 200
 
-# @given('a user Nicolas')
-# def step_given(context):
-#     user = User(username= "Nicolas", email="Nicolas@gmail.com", password="123")
-#     add_user(user.username, user.password, user.email)
-#     context.user = user
-#     assert True
+@then('this figure is created for the user x')
+def step_impl(context):
+    figure = Figure_infantry.query.filter_by(id_game= 1, id_user= 1).first()
+    assert figure.figure_type == 1
 
-# @when('Shoot')
-# def step_when(context):
-#     user2 = User(username= "Ignacio", email="Ignacio@gmail.com", password="123")
-#     Figure_infantry(id_game= 1, id_user= 2, hp=10, velocidad=3, tamaño=1, direccion=0,pos_x=0, pos_y=0, type=1)
-#     game = Game_Infantry(id_user1= context.user, id_user2= user2.id)
-#     context.page = context.client.post(url_for("infantry.shoot_entity",direction= 1, figure_id=1, user_id= 2, game_id= 1))
-#     print(context.page.status_code)
-#     assert context.page.status_code == 200
+@given('a user y')
+def step_impl(context):
+    headers = {"Content-Type": "application/json"}
+    context.page = context.client.post(url_for("users.create_user"), data = json.dumps(
+        {"username" : "Jose", "email" : "Jose@gmail.com", "password" : "123"}), headers=headers)
+    assert context.page.status_code == 200
 
-  
-# @then(u'create the projectile')
-# def step_then(context) :
-#     #figure = Figure_infantry.query.filter_by(id_game = 1, id_user = 1).first()
-#     #assert figure.pos_x == 1
-#     pass
- 
+@when('you select the type 2 figure')
+def step_impl(context):
+    #The game must have a user 2
+    game = Game_Infantry(id_user1= 1, id_user2= 2)
+    db.session.add(game)
+    db.session.commit() 
+    headers = {"Content-Type": "application/json"}
+    context.page = context.client.post(url_for("infantry.choose_figure", game_id = 1, user_id = 1, type= "2"), data = json.dumps(
+        {"pos_x" : 1, "pos_y" : 1}), headers=headers)
+    assert context.page.status_code == 200
+
+@then('this figure is created for the user y')
+def step_impl(context):
+    figure = Figure_infantry.query.filter_by(id_game= 1, id_user= 1).first()
+    assert figure.figure_type == 2
+
+@given('a user z')
+def step_impl(context):
+    headers = {"Content-Type": "application/json"}
+    context.page = context.client.post(url_for("users.create_user"), data = json.dumps(
+        {"username" : "Felipe", "email" : "Felipe@gmail.com", "password" : "123"}), headers=headers)
+    assert context.page.status_code == 200
+
+@when('you select the type 3 figure')
+def step_impl(context):
+    #The game must have a user 2
+    game = Game_Infantry(id_user1= 1, id_user2= 2)
+    db.session.add(game)
+    db.session.commit() 
+    headers = {"Content-Type": "application/json"}
+    context.page = context.client.post(url_for("infantry.choose_figure", game_id = 1, user_id = 1, type= "3"), data = json.dumps(
+        {"pos_x" : 1, "pos_y" : 1}), headers=headers)
+    assert context.page.status_code == 200
+
+@then('this figure is created for the user z')
+def step_impl(context):
+    figure = Figure_infantry.query.filter_by(id_game= 1, id_user= 1).first()
+    assert figure.figure_type == 3
+
+@given('a user t')
+def step_impl(context):
+    headers = {"Content-Type": "application/json"}
+    context.page = context.client.post(url_for("users.create_user"), data = json.dumps(
+        {"username" : "Luis", "email" : "Luis@gmail.com", "password" : "123"}), headers=headers)
+    assert context.page.status_code == 200
+
+@when('you select the type 4 figure')
+def step_impl(context):
+    #The game must have a user 2
+    game = Game_Infantry(id_user1= 1, id_user2= 2)
+    db.session.add(game)
+    db.session.commit() 
+    headers = {"Content-Type": "application/json"}
+    context.page = context.client.post(url_for("infantry.choose_figure", game_id = 1, user_id = 1, type= "4"), data = json.dumps(
+        {"pos_x" : 1, "pos_y" : 1}), headers=headers)
+    assert context.page.status_code == 200
+
+@then('this figure is created for the user t')
+def step_impl(context):
+    figure = Figure_infantry.query.filter_by(id_game= 1, id_user= 1).first()
+    assert figure.figure_type == 4
+
+############################################## PROYECTIL ###########################################
+@given('figure 1')
+def step_impl(context):
+     #The game must have a user 2
+    game = Game_Infantry(id_user1= 1, id_user2= 2)
+    db.session.add(game)
+    db.session.commit() 
+    headers = {"Content-Type": "application/json"}
+    context.page = context.client.post(url_for("infantry.choose_figure", game_id = 1, user_id = 1, type= "1"), data = json.dumps(
+        {"pos_x" : 1, "pos_y" : 1}), headers=headers)
+    assert context.page.status_code == 200
+
+@when('he shoots for figure 1')
+def step_impl(context):
+    headers = {"Content-Type": "application/json"}
+    context.page = context.client.post(url_for("infantry.shoot_entity", game_id = 1, user_id =  1, direccion= 1), data = json.dumps(
+        {"velocity" : 1}), headers=headers)
+    assert context.page.status_code == 200
+
+@then('your projectile is created for figure 1')
+def step_impl(context):
+    #Since the projectile was created successfully in the previous step then the scenario is true
+    assert True
+
+@given('figure 2')
+def step_impl(context):
+    #The game must have a user 2
+    game = Game_Infantry(id_user1= 1, id_user2= 2)
+    db.session.add(game)
+    db.session.commit() 
+    headers = {"Content-Type": "application/json"}
+    context.page = context.client.post(url_for("infantry.choose_figure", game_id = 1, user_id = 1, type= "2"), data = json.dumps(
+        {"pos_x" : 1, "pos_y" : 1}), headers=headers)
+    assert context.page.status_code == 200
+
+@when('he shoots for figure 2')
+def step_impl(context):
+    headers = {"Content-Type": "application/json"}
+    context.page = context.client.post(url_for("infantry.shoot_entity", game_id = 1, user_id =  1, direccion= 1), data = json.dumps(
+        {"velocity" : 1}), headers=headers)
+    assert context.page.status_code == 200
+
+@then('your projectile is created for figure 2')
+def step_impl(context):
+    #Since the projectile was created successfully in the previous step then the scenario is true
+    assert True
+
+@given('figure 3')
+def step_impl(context):
+    #The game must have a user 2
+    game = Game_Infantry(id_user1= 1, id_user2= 2)
+    db.session.add(game)
+    db.session.commit() 
+    headers = {"Content-Type": "application/json"}
+    context.page = context.client.post(url_for("infantry.choose_figure", game_id = 1, user_id = 1, type= "3"), data = json.dumps(
+        {"pos_x" : 1, "pos_y" : 1}), headers=headers)
+    assert context.page.status_code == 200
+
+@when('he shoots for figure 3')
+def step_impl(context):
+    headers = {"Content-Type": "application/json"}
+    context.page = context.client.post(url_for("infantry.shoot_entity", game_id = 1, user_id =  1, direccion= 1), data = json.dumps(
+        {"velocity" : 1}), headers=headers)
+    assert context.page.status_code == 200
+
+@then('your projectile is created for figure 3')
+def step_impl(context):
+    #Since the projectile was created successfully in the previous step then the scenario is true
+    assert True
+
+@given('figure 4')
+def step_impl(context):
+    #The game must have a user 2
+    game = Game_Infantry(id_user1= 1, id_user2= 2)
+    db.session.add(game)
+    db.session.commit() 
+    headers = {"Content-Type": "application/json"}
+    context.page = context.client.post(url_for("infantry.choose_figure", game_id = 1, user_id = 1, type= "4"), data = json.dumps(
+        {"pos_x" : 1, "pos_y" : 1}), headers=headers)
+    assert context.page.status_code == 200
+
+@when('he shoots for figure 4')
+def step_impl(context):
+    headers = {"Content-Type": "application/json"}
+    context.page = context.client.post(url_for("infantry.shoot_entity", game_id = 1, user_id =  1, direccion= 1), data = json.dumps(
+        {"velocity" : 4}), headers=headers)
+    assert context.page.status_code == 200
+
+@then('your projectile is created figure 4')
+def step_impl(context):
+    #Since the projectile was created successfully in the previous step then the scenario is true
+    assert True
+
+@given('a figure')
+def step_impl(context):
+     #The game must have a user 2
+    game = Game_Infantry(id_user1= 1, id_user2= 2)
+    db.session.add(game)
+    db.session.commit() 
+    headers = {"Content-Type": "application/json"}
+    context.page = context.client.post(url_for("infantry.choose_figure", game_id = 1, user_id = 1, type= "1"), data = json.dumps(
+        {"pos_x" : 1, "pos_y" : 1}), headers=headers)
+    assert context.page.status_code == 200
+
+@when('it moves')
+def step_impl(context):
+    headers = {"Content-Type": "application/json"}
+    context.page = context.client.post(url_for("infantry.mov_action"), data = json.dumps(
+        {"velocity" : 1 ,"course" : 3 ,"game_id" : 1 ,"user_id" : 1}), headers=headers)
+    figure = Figure_infantry.query.filter_by(id_game = 1, id_user= 1).first()
+    print(figure.pos_x)
+    print(figure.pos_y)
+    assert context.page.status_code == 202
+
