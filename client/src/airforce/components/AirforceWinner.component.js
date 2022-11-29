c4c5ff518bcd831379f03e789c17bb26acfcecfa
joaquinmoran <joacomoran6@gmail.com>
2022-11-29 11:34:04 -0300

    use the getUserBoard to save the name of the winner

--- client/src/airforce/components/AirforceWinner.component.js
+++ client/src/airforce/components/AirforceWinner.component.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
+import userService from "../../services/user.service";
 import "./AirforceWinner.css";
 
 
 export default class AirforceWinner extends Component {
     render(){
-        const userWinner = localStorage.getItem("userWinner");
+        userService.getUserBoard(localStorage.getItem("userWinner")).then(
+            (response) => {
+                localStorage.setItem("winner", response.data.username)
+            }
+        )
+        const userWinner = localStorage.getItem("winner");
         return(
             <div className="winner">
                 <h1>
