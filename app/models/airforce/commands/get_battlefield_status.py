957f137faaaaf356cddca1e98bb3e1cf4e86df86
joaquinmoran <joacomoran6@gmail.com>
2022-11-29 12:31:28 -0300

    add a player attribute

--- app/models/airforce/commands/get_battlefield_status.py
+++ app/models/airforce/commands/get_battlefield_status.py
@@ -1,12 +1,14 @@
 class GetBattlefieldStatus:
     battlefield = None
     air_force_game = None
+    player = None
 
-    def __init__(self, battlefield, air_force_game):
+    def __init__(self, battlefield, air_force_game, player):
         self.battlefield = battlefield
         self.air_force_game = air_force_game
+        self.player = player
 
     def execute(self):
         if self.air_force_game.game_ended():
             return {"status": "end", "Winner": self.air_force_game.winner()}
-        return self.battlefield.get_status()
+        return self.battlefield.get_status_player(self.player)
